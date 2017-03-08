0f4aa50a478e3e26b454267f81c2c5e000e1156e
deepankar <deepankar@promactinfo.com>
2017-03-08 18:01:38 +0530

    Added Code Snippet Question Model

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Question/CodingLanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.Models.Question
+{
+    public class CodingLanguage: BaseModel
+    { 
+        [Required]
+        public string Language { get; set; }
+
+    }
+}
---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Question/QuestionLanguageMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.Models.Question
+{
+    public class QuestionLanguageMapping: BaseModel
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public int LanguageId { get; set; }
+
+        [ForeignKey("QuestionId")]
+        public CodeSnippetQuestion codeSnippetQuestion { get; set; }
+
+        [ForeignKey("LanguageId")]
+        public CodingLanguage codeLanguage { get; set; }
+    }
+}
