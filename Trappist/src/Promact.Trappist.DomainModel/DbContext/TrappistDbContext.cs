c1f3001aaf7ceae39b4b935b780753ad9c1e8c7c
partha <partha@PR2C6R4Y2016>
2017-03-08 15:50:52 +0530

    Catagory Model Implemented

--- Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
+++ Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Promact.Trappist.Web.Models;
 using Promact.Trappist.DomainModel.Models.Question;
+using Promact.Trappist.DomainModel.Models.Catagory;
 
 namespace Promact.Trappist.Web.Data
 {
@@ -25,6 +26,7 @@ namespace Promact.Trappist.Web.Data
         }
 
         public DbSet<Question> Question { get; set; }
+        public DbSet <Catagory> Catagorys { get; set; }
         public DbSet<CodeSnippetQuestion> CodeSnippetQuestion { get; set; }
         public DbSet<CodingLanguage> CodingLanguage { get; set; }
         public DbSet<QuestionLanguageMapping> QuestionLanguageMapping { get; set; }
