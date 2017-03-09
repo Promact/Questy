b47d94632af53563c7b1c4e7644be321d049eb6a
partha <partha@PR2C6R4Y2016>
2017-03-09 13:01:04 +0530

    Catagory model Updated

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Category/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.Models.Category
+{
+    public class Category : BaseModel
+    {
+        [Required]
+        [MaxLength(150)]
+        public string categoryName { get; set; }
+    }
+}
