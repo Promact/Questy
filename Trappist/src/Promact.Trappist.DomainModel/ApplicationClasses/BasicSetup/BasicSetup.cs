a2e0468959851360bf8af349f5f089decc9794ba
hardikpatel <hardikpatel@PR2C4R2Y2015>
2017-03-09 18:07:01 +0530

    Moved classe in basic setup folder

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/BasicSetup/BasicSetup.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/BasicSetup/BasicSetup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,30 @@ namespace Promact.Trappist.DomainModel.ApplicationClasses.BasicSetup
 {
     public class BasicSetup
     {
+        [Required]
+        [StringLength(150)]
+        [EmailAddress]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(14, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public EmailSettings EmailSettings { get; set; }
+        public ConnectionStringParamters ConnectionString { get; set; }
     }
 }
