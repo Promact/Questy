c0135a8e41201e740e572038f40b6db04c4439d2
partha <partha@PR2C6R4Y2016>
2017-03-09 13:01:04 +0530

    Catagory model Updated

--- Trappist/src/Promact.Trappist.Web/Migrations/20170309072816_CatagoryModelMigration.Designer.cs
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170309072816_CatagoryModelMigration.Designer.cs
@@ -8,9 +8,10 @@ using Promact.Trappist.Web.Data;
 namespace Promact.Trappist.Web.Migrations
 {
     [DbContext(typeof(TrappistDbContext))]
-    partial class TrappistDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170309072816_CatagoryModelMigration")]
+    partial class CatagoryModelMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
