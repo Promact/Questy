7b98bb7e074309884824c3626d249b97b455ef41
Roshni Shah <shah.roshni187@gmail.com>
2017-03-09 16:57:03 +0530

    Added application classes folder heirarchy

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/BasicSetup/BasicSetup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.BasicSetup
+{
+    public class BasicSetup
+    {
+    }
+}
