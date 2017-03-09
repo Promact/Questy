d92496cf6321b64581ff4c3df2a0261181016edd
hardikpatel <hardikpatel@PR2C4R2Y2015>
2017-03-09 17:56:42 +0530

    Added BasicSetup Added ConnectionStringParamters Added EmailResponse Added EmailSettings

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/BasicSetup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses
+{
+    public class BasicSetup
+    {
+        [Required]
+        [StringLength(150)]
+        [EmailAddress]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(14, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public EmailSettings EmailSettings { get; set; }
+        public ConnectionStringParamters ConnectionString { get; set; }
+    }
+}
