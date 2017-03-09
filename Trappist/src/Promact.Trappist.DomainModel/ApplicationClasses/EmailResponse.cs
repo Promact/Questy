451a48dacdb132ee9c2f13493bbcaa9507385ae1
hardikpatel <hardikpatel@PR2C4R2Y2015>
2017-03-09 17:56:42 +0530

    Added BasicSetup Added ConnectionStringParamters Added EmailResponse Added EmailSettings

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/EmailResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses
+{
+    public class EmailResponse
+    {
+        public bool IsMailSent { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
