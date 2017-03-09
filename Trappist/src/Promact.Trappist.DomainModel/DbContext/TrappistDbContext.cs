b2e202b08102edfec523b68a1e4f1370b23ef848
partha <partha@PR2C6R4Y2016>
2017-03-09 13:01:04 +0530

    Catagory model Updated

--- Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
+++ Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Promact.Trappist.Web.Models;
 using Promact.Trappist.DomainModel.Models.Question;
 using Promact.Trappist.DomainModel.Models.Category;
-using Promact.Trappist.DomainModel.Models;
 
 namespace Promact.Trappist.Web.Data
 {
