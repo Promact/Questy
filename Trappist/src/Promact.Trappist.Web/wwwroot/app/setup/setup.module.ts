ed2c3673775e1bad54f79472562f571b87562540
Roshni Shah <shah.roshni187@gmail.com>
2017-03-09 15:01:22 +0530

    added another angular app for basic setup feature

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/setup/setup.module.ts
@@ -0,0 +1,24 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import 'rxjs/Rx';
+
+import { SetupComponent } from './setup.component';
+
+import { SharedModule } from "../shared/shared.module";
+import { CoreModule } from "../core/core.module";
+
+@NgModule({
+    bootstrap: [SetupComponent],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        CoreModule       
+    ],
+    providers: [
+    ],
+    declarations: [
+        SetupComponent
+    ]
+})
+export class SetupModule { }
