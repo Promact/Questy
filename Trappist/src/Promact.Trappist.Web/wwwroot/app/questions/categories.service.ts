800f11ce2a9e14831f1308fc84b339ee2c0548cb
partha <partha@PR2C6R4Y2016>
2017-03-10 15:34:51 +0530

    Add and Edit API added for server Side.

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/categories.service.ts
@@ -0,0 +1,20 @@
+﻿import { Injectable } from "@angular/core";
+import { HttpService } from "../core/http.service";
+import { Category } from "./category.model";
+
+@Injectable()
+
+export class CategoriesService
+{
+
+    private categoriesApiUrl = "api/Categories";
+
+    constructor(private httpService: HttpService) {
+
+    }
+
+    addCategory(category:Category)
+    {
+        return this.httpService.post("this.categoriesApiUrl",category);
+    }
+}
