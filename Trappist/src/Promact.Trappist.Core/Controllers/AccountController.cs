b8d9b8ee65b4806c79db16219114d637448b3fe3
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 09:49:54 +0530

    Updated login model in application class

--- Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
+++ Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Promact.Trappist.DomainModel.ApplicationClasses.Account;
 using Promact.Trappist.Web.Models;
 using System;
 using System.Collections.Generic;
