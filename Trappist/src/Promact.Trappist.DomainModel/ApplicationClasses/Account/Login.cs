1c37671bb8d2938fdece5329d5015f9f02a3bbe6
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 09:49:54 +0530

    Updated login model in application class

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Promact.Trappist.DomainModel.ApplicationClasses.Account
 {
