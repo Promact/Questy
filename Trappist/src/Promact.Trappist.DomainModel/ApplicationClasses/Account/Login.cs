1d05593a10e3133cf7400dfb992fe35f8269d4aa
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 16:05:43 +0530

    Login validation functionality updated

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Promact.Trappist.DomainModel.ApplicationClasses.Account
 {
     public class Login
     {
-        //Attributes requred for login purpose
-        [EmailAddress]
-        [Required(ErrorMessage = "Please Enter UserId or EmailId")]
-        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please Enter Correct Email Address")]
+        
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please Enter Proper Format EmailId")]
         public string Email { get; set; }
 
-
-        [Required(ErrorMessage = "Please Enter Password")]
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
