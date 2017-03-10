805b008d90528a91a205778ed7b198c6eaf521e6
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 08:44:52 +0530

    login Model & account controller are created

--- Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
+++ Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Promact.Trappist.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,30 @@ using System.Threading.Tasks;
 
 namespace Promact.Trappist.Core.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
-    {
-        public IActionResult Login()
+    { 
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(ApplicationUser model)
+        {
+            if (ModelState.IsValid)
+            { }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
         }
 
         public IActionResult ForgotPassword()
