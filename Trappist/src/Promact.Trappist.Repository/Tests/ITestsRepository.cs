a4b2982d2f121489515313be78289fc7799c08fe
niharika <niharika@ADMINRG-4HGCIQ2>
2017-03-10 16:47:26 +0530

    rectified conventions

--- Trappist/src/Promact.Trappist.Repository/Tests/ITestsRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Tests/ITestsRepository.cs
@@ -1,11 +1,10 @@
 ﻿using Promact.Trappist.DomainModel.Models.Test;
-
 namespace Promact.Trappist.Repository.Tests
 {
     public interface ITestsRepository
     {
-        void createTest(Test t1);// for test creation
-        bool uniqueName(Test t); //to check whether test name is unique or not
+        void CreateTest(Test testObj);// for test creation
+        bool UniqueName(Test test); //to check whether test name is unique or not
 
     }
 
