a39a19d86c1b322020abdfde4b55e2eb24a10d28
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 16:05:43 +0530

    Login validation functionality updated

--- Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
+++ Trappist/src/Promact.Trappist.Core/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Promact.Trappist.DomainModel.ApplicationClasses.Account;
 using Promact.Trappist.Web.Models;
 using System;
@@ -12,29 +13,40 @@ namespace Promact.Trappist.Core.Controllers
 {
     [Authorize]
     public class AccountController : Controller
-    { 
-        // GET: /Account/Login
-        //[HttpGet]
-        //[AllowAnonymous]
-        //public IActionResult Login(string returnUrl = null)
-        //{
-        //    ViewData["ReturnUrl"] = returnUrl;
-        //    return View();
-        //}
+    {
+
+        /// <summary>
+        /// Login via Get method
+        /// </summary>
+        /// <returns>Login form view</returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        /// <summary>
+        /// Login via Get method
+        /// </summary>
+        /// <returns>Invalid login attempt</returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(Login model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            { }
+            else {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+         
+            return View(model);
+        }
 
 
-        //[HttpPost]
-        //[AllowAnonymous]
-        //public IActionResult Login(Login model)
-        //{
-        //    if (ModelState.IsValid)
-        //    { }
-        //    else
-        //    {
-        //        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-        //    }
-        //    return View(model);
-        //}
 
         public IActionResult ForgotPassword()
         {
