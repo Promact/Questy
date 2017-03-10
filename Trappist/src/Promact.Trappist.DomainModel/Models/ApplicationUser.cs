8212f0ceee36a983e550ad7cbcb33d04c61f6038
Suaprna Acharya <suparna@promactinfo.com>
2017-03-10 08:44:52 +0530

    login Model & account controller are created

--- Trappist/src/Promact.Trappist.DomainModel/Models/ApplicationUser.cs
+++ Trappist/src/Promact.Trappist.DomainModel/Models/ApplicationUser.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Promact.Trappist.Web.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+
+
+
     }
 }
