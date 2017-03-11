3261ef5ea7890982d5216dd3796bfafc624dc88f
Rushi Soni <rushi.2106@gmail.com>
2017-03-11 19:03:02 +0530

    update .travis.yml to build docker image, added dockerfile, added black node_modules for publishing

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+cd ./Trappist/src/Promact.Trappist.Web
+dotnet publish -o published
+cd ../../../
+docker build -t promact/trappist:$TRAVIS_BRANCH .
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
+docker push promact/trappist:$TRAVIS_BRANCH
