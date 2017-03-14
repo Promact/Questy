4dcd92a82757fd4ccd23f48e6d9e24ae42b7c126
Vihar Sanghavi <vihar@promactinfo.com>
2017-03-14 08:46:31 +0530

    Create server side API for single multiple answer question

---
+++ Trappist/src/Promact.Trappist.Core/Controllers/QuestionsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Promact.Trappist.DomainModel.Models.Question;
+using Promact.Trappist.Repository.Questions;
+using System;
+
+namespace Promact.Trappist.Core.Controllers
+{
+    [Route("api/question")]
+    public class QuestionsController : Controller
+    {
+        private readonly IQuestionRepository _questionsRepository;
+        public QuestionsController(IQuestionRepository questionsRepository)
+        {
+            _questionsRepository = questionsRepository;
+        }
+
+        [HttpGet]
+        /// <summary>
+        /// Gets all questions
+        /// </summary>
+        /// <returns>Questions list</returns>   
+        public IActionResult GetQuestions()
+        {
+            var questions = _questionsRepository.GetAllQuestions();
+            return Json(questions);
+        }
+        [HttpPost]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="singleMultipleAnswerQuestion"></param>
+        /// <returns></returns>
+        public IActionResult AddSingleMultipleAnswerQuestion(SingleMultipleAnswerQuestion singleMultipleAnswerQuestion)
+        {
+            _questionsRepository.AddSingleMultipleAnswerQuestion(singleMultipleAnswerQuestion);
+            return Ok();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="singleMultipleAnswerQuestion"></param>
+        /// <returns></returns>
+        public IActionResult AddSingleMultipleAnswerQuestionOption(SingleMultipleAnswerQuestionOption singleMultipleAnswerQuestionOption)
+        {
+            _questionsRepository.AddSingleMultipleAnswerQuestionOption(singleMultipleAnswerQuestionOption);
+            return Ok();
+        }
+    }
+}
