95cd72146cc7a7903010f8167462dfc96fd3123f
deepankar <deepankar@promactinfo.com>
2017-03-14 16:42:29 +0530

    Removed unnecessary Apllication class

--- Trappist/src/Promact.Trappist.Repository/Questions/QuestionRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Questions/QuestionRepository.cs
@@ -28,7 +28,7 @@ namespace Promact.Trappist.Repository.Questions
             try
             {
                 _dbContext.CodeSnippetQuestion.Add(codeSnippetQuestion);
-                await _dbContext.SaveChangesAsync();
+                _dbContext.SaveChangesAsync();
             }
             catch (DbUpdateException)
             {
