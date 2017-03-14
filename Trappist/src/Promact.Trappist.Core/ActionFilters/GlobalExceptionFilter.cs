1f3d4ca9106aaf0ea9f031540566d51b37fb732c
kunal <kunal@PR1C4R3Y2016>
2017-03-14 16:57:15 +0530

    gloabl exception filter added

---
+++ Trappist/src/Promact.Trappist.Core/ActionFilters/GlobalExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Promact.Trappist.Core.ActionFilters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        private ILogger<GlobalExceptionFilter> _logger;
+
+        public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Method fire when exception will thrown
+        /// </summary>
+        /// <param name="context">context object</param>
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.ToString());
+        }
+    }
+}
