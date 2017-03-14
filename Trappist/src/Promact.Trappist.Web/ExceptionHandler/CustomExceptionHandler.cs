9358b18bd446484f762a8bf0fd935b0a4974712c
kunal <kunal@PR1C4R3Y2016>
2017-03-14 13:56:08 +0530

    global exception handler added.

---
+++ Trappist/src/Promact.Trappist.Web/ExceptionHandler/CustomExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Promact.Trappist.Web.ExceptionHandler
+{
+    public class CustomExceptionHandler : IExceptionFilter
+    {
+        private ILogger<CustomExceptionHandler> _logger;
+
+        public CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Method fire when exception will thrown
+        /// </summary>
+        /// <param name="context">context object</param>
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.ToString());
+        }
+    }
+}
