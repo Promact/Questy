f590f84940f5efdb02e7d5086309aafb2cb734c3
deepankar <deepankar@promactinfo.com>
2017-03-14 14:58:39 +0530

    Extra tab removed

--- Trappist/src/Promact.Trappist.Web/Startup.cs
+++ Trappist/src/Promact.Trappist.Web/Startup.cs
@@ -120,7 +120,9 @@ namespace Promact.Trappist.Web
                      name: "spa-fallback",
                      defaults: new { controller = "Home", action = "Index" });
             });
-			context.Seed();
+			
+	        context.Seed();
+
         }
     }
 }
