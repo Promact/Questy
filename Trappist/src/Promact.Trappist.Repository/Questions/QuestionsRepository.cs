51bcba79b2fc1c7bb164b2af81b57b5bdcbc59e5
deepankar <deepankar@promactinfo.com>
2017-03-14 16:19:15 +0530

    Added methods in IQuestionRepository and QuestionRepository

--- Trappist/src/Promact.Trappist.Repository/Questions/QuestionsRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Questions/QuestionsRepository.cs
@@ -2,6 +2,8 @@
 using Promact.Trappist.DomainModel.Models.Question;
 using System.Linq;
 using Promact.Trappist.DomainModel.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Promact.Trappist.Repository.Questions
 {
@@ -12,6 +14,28 @@ namespace Promact.Trappist.Repository.Questions
         public QuestionsRepository(TrappistDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Add new code snippet question to the database
+        /// </summary>
+        /// <param name="codeSnippetQuestion">Code Snippet Question Model</param>
+        /// <returns>
+        /// returns 0 : When code snippet question is added successfully to the database
+        /// returns 1 : When code snippet question failed to add 
+        /// </returns>
+        public async Task<int> AddCodeSnippetQuestion(CodeSnippetQuestion codeSnippetQuestion)
+        {
+            try
+            {
+                _dbContext.CodeSnippetQuestion.Add(codeSnippetQuestion);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return 0;
+            }
+            return 1;
         }
 
         /// <summary>
