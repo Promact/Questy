5c0cf98b6ee9b375a090b143e7f3e5e81e341fdf
deepankar <deepankar@promactinfo.com>
2017-03-14 15:01:10 +0530

    Formatting issue solved

--- Trappist/src/Promact.Trappist.Web/Startup.cs
+++ Trappist/src/Promact.Trappist.Web/Startup.cs
@@ -115,9 +115,7 @@ namespace Promact.Trappist.Web
                      name: "spa-fallback",
                      defaults: new { controller = "Home", action = "Index" });
             });
-			
-	        context.Seed();
-
+			context.Seed();
         }
     }
 }
