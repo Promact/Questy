f1ebee05ca06044fa88db8d52e20a540b1403ad7
deepankar <deepankar@promactinfo.com>
2017-03-14 09:26:36 +0530

    Added application class for Code snippet question

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodeSnippetQuestionModel.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodeSnippetQuestionModel.cs
@@ -1,12 +1,26 @@
 ﻿using Promact.Trappist.DomainModel.Models.Enums;
+using System.Collections.Generic;
 
 namespace Promact.Trappist.DomainModel.ApplicationClasses.Question
 {
     public class CodeSnippetQuestionModel
     {
         public QuestionType QuestionType { get; set;}
+
         public DifficultyLevel  DifficultyLevel { get; set; }
+
         public string QuestionDetail { get; set; }
 
+        public ICollection<CodingLanguageModel> ProgramingLanguage { get; set; }
+
+        public bool CheckCodeComplexity { get; set; }
+
+        public bool CheckTimeComplexity { get; set; }
+
+        public bool RunBasicTestCase { get; set; }
+
+        public bool RunCornerTestCase { get; set; }
+
+        public bool RunNecessaryTestCase { get; set; }
     }
 }
