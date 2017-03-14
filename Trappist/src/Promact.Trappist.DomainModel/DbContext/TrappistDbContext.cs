a6f87fa2ca135232c54ccda97ae8a4b4ae0cde11
Deepankar Sinha <deepankar@promactinfo.com>
2017-03-14 14:24:46 +0530

    Spacing issue resolved

--- Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
+++ Trappist/src/Promact.Trappist.DomainModel/DbContext/TrappistDbContext.cs
@@ -28,7 +28,7 @@ namespace Promact.Trappist.DomainModel.DbContext
         public DbSet<SingleMultipleAnswerQuestion> SingleMultipleAnswerQuestion { get; set; }
         public DbSet<SingleMultipleAnswerQuestionOption> SingleMultipleAnswerQuestionOption { get; set; }
 		
-		public DbSet<CodeSnippetQuestion> CodeSnippetQuestion { get; set; }
+	public DbSet<CodeSnippetQuestion> CodeSnippetQuestion { get; set; }
         public DbSet<CodingLanguage> CodingLanguage { get; set; }
         public DbSet<QuestionLanguageMapping> QuestionLanguageMapping { get; set; }
         
