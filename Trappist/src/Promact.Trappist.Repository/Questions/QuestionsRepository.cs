95833655fdd3f3f3311af7a60647230a29a90f7e
sandipan <sandipan@PR2C1R6Y2016>
2017-03-14 08:44:05 +0530

    made seperate repository and controller for category

--- Trappist/src/Promact.Trappist.Repository/Questions/QuestionsRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Questions/QuestionsRepository.cs
@@ -17,14 +17,6 @@ namespace Promact.Trappist.Repository.Questions
         }
        
         /// <summary>
-        /// Get all the names of Categories
-        /// </summary>
-        /// <returns>Categories list</returns>
-        public IEnumerable<string> GetAllCategories()
-        {
-            return (_dbContext.Category.Select(x=>x.CategoryName).ToList());
-        }
-        /// <summary>
         /// Get all questions
         /// </summary>
         /// <returns>Question list</returns>
