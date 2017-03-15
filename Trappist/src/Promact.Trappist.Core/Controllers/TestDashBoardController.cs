d6739cdfec836570d846bd9232146612e2417e52
asif <asif@PR2C2R5Y2015>
2017-03-15 12:51:15 +0530

    Created TestDashBoard Controller, TestDashBoardRepository,ITestDashBoardRepository, Server Side Api Implemented For TestDashBoard

---
+++ Trappist/src/Promact.Trappist.Core/Controllers/TestDashBoardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Promact.Trappist.Repository.TestDashBoard;
+
+namespace Promact.Trappist.Core.Controllers
+{
+    [Route("api/TestDashboard")]
+    public class TestDashBoardController : Controller
+    {
+      
+     
+            public ITestDashBoardRepository _testDisplay;
+           
+            public TestDashBoardController(ITestDashBoardRepository testDisplay)
+            {
+                _testDisplay = testDisplay;
+             
+            }
+
+            /// <summary>
+            /// Get All Tests
+            /// </summary>
+            /// <returns>List of Tests</returns>
+            [HttpGet]
+            public IActionResult GetAllTest()
+            {
+                var Tests = _testDisplay.GetAllTests();
+
+                return Json(Tests);
+            }
+
+
+        }
+}
