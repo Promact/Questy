6001fadff1bd2aec127fd31433a64cd7595dc5ac
Suaprna Acharya <suparna@promactinfo.com>
2017-03-15 16:48:32 +0530

    Login updated v4

---
+++ Trappist/src/Promact.Trappist.Repository/Account/LoginRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Promact.Trappist.Web.Models;
+using System.Threading.Tasks;
+using System;
+using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.ApplicationClasses.Account;
+
+namespace Promact.Trappist.Repository.Account
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly TrappistDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LoginRepository(SignInManager<ApplicationUser> signInManager, TrappistDbContext dbcontext)
+        {
+            _signInManager = signInManager;
+            _context = dbcontext;
+
+        }
+        /// <summary>
+        /// this method is used to check email and password with database
+        /// </summary>
+        /// <param name="loginModel">object of Login</param>
+        /// <returns>true if matched</returns>
+        public async Task<bool> LoginUser(Login loginModel)
+        {
+            try
+            {
+                var search = await _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, isPersistent: true, lockoutOnFailure: true);
+                if (search.Succeeded)
+                {
+                    return true;
+                }
+                else
+                {                   
+                    return false;
+                }
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+        }
+
+    }
+
+
+}
+
+
+
