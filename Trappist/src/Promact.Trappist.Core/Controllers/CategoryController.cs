5cc86476db2b285754046ce827af3cf857d00090
partha <partha@PR2C6R4Y2016>
2017-03-15 15:37:35 +0530

    Rebased with dev branch

--- Trappist/src/Promact.Trappist.Core/Controllers/CategoryController.cs
+++ Trappist/src/Promact.Trappist.Core/Controllers/CategoryController.cs
@@ -68,50 +68,5 @@ namespace Promact.Trappist.Core.Controllers
             #endregion
         }
 
-        #region post Method 
-        [HttpPost]
-        /// <summary>
-        /// Post Method 
-        /// Will Add a Catagory Name in Category Model
-        ///</summary>
-        /// <param name="category">Object of  class Category</param>
-        /// <returns>object of the class </returns>
-        public IActionResult CatagoryAdd([FromBody] Category category)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            _categoryRepository.AddCategory(category);
-            return Ok(category);
-        }
-        #endregion
-
-        #region PutMethod
-        /// <summary>
-        /// Put Method
-        /// Will Edit a Existing Category from Category Table
-        /// </summary>
-        /// <param name="Id">Id is the primary key of Category Model</param>
-        /// <param name="catagory">Object of  class Category</param>
-        /// <returns>object of the class if key found or else it will return Bad request</returns>
-        [HttpPut("{id}")]
-        public IActionResult CategoryEdit(int Id, [FromBody] Category category)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var previousCategory = _categoryRepository.GetCategory(Id);
-            if (previousCategory == null)
-            {
-                return NotFound();
-            }
-            previousCategory.CategoryName = category.CategoryName;
-            _categoryRepository.CategoryEdit(previousCategory);
-            return Ok(category);
-            #endregion
-        }
-
     }
 }
