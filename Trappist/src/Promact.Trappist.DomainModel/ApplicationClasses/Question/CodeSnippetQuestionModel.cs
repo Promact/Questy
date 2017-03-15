9e0e02aadfaf94add9eeed1af5d0f0a0383ce577
deepankar <deepankar@promactinfo.com>
2017-03-15 09:35:05 +0530

    updated AddCodeSnippetQuestion in QuestionRepository

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodeSnippetQuestionModel.cs
@@ -0,0 +1,36 @@
+﻿using Promact.Trappist.DomainModel.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.Question
+{
+    /// <summary>
+    ///Application class for code snippet question 
+    /// </summary>
+    public class CodeSnippetQuestionModel
+    {
+        public int CategoryId { get; set; }
+
+        public QuestionType QuestionType { get; set;}
+
+        public DifficultyLevel  DifficultyLevel { get; set; }
+
+        public string QuestionDetail { get; set; }
+
+        public ICollection<CodingLanguageModel> ProgramingLanguage { get; set; }
+
+        public bool CheckCodeComplexity { get; set; }
+
+        public bool CheckTimeComplexity { get; set; }
+
+        public bool RunBasicTestCase { get; set; }
+
+        public bool RunCornerTestCase { get; set; }
+
+        public bool RunNecessaryTestCase { get; set; }
+
+        public string CreateBy { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
