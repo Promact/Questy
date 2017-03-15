520709ad9309aec5860dd04ba14d95dc38bb797f
Roshni Shah <shah.roshni187@gmail.com>
2017-03-15 17:29:55 +0530

    Unit testing bootstrap with xUnit and Collection Fixtures

---
+++ Trappist/src/Promact.Trappist.Test/BasicSetup/BasicSetupRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Promact.Trappist.Test.BasicSetup
+{
+    [Collection("Register Dependency")]
+    public class BasicSetupRepositoryTest
+    {
+        private readonly Bootstrap _bootstrap;
+
+        public BasicSetupRepositoryTest(Bootstrap bootstrap)
+        {
+            _bootstrap = bootstrap;
+        }
+    }
+}
