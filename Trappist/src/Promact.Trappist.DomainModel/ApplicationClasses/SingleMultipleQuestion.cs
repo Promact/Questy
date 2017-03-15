4990291a70a32f95d38a27ca88d432c44ac9b5fc
Vihar Sanghavi <vihar@promactinfo.com>
2017-03-15 11:57:07 +0530

    Solving issue for post request

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/SingleMultipleQuestion.cs
@@ -0,0 +1,14 @@
+﻿using Promact.Trappist.DomainModel.Models.Question;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses
+{
+    public class SingleMultipleQuestion
+    {
+        public SingleMultipleAnswerQuestion singleMultipleAnswerQuestion { get; set; }
+        public SingleMultipleAnswerQuestionOption singleMultipleAnswerQuestionOption { get; set; }
+    }
+}
