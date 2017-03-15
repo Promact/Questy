f2fc3bdebbcc2ab6471d17e619eab41b54e770fc
deepankar <deepankar@promactinfo.com>
2017-03-15 09:50:47 +0530

    Added CodingLanguageModel class

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodingLanguageModel.cs
@@ -0,0 +1,13 @@
+﻿using Promact.Trappist.DomainModel.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.Question
+{
+    public class CodingLanguageModel
+    {
+        public ProgramingLanguage Language { get; set; }
+    }
+}
