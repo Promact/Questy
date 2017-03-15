083bc4400e4918cd8e4135c542fefb207f537002
karan mehta <karan.mehta@promactinfo.com>
2017-03-15 19:20:15 +0530

    client side add function for remove category in questions-dashboard.component.ts add function for send http(delete) request in categories.service.ts

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
@@ -34,7 +34,9 @@ export class QuestionsDashboardComponent {
     addCategoryDialog() {
         this.dialog.open(AddCategoryDialogComponent);
     }
-
+    removeCategory(categoryName: string) {
+        this.categoryService.removeCategory(categoryName).subscribe((Response) => Response.json());
+    }
 }
 
 @Component({
