4a1671d8521134f46e66e17144d3f5ab370856af
sandipan <sandipan@PR2C1R6Y2016>
2017-03-15 08:22:39 +0530

    Corrected Repository spelling

--- Trappist/src/Promact.Trappist.Web/Startup.cs
+++ Trappist/src/Promact.Trappist.Web/Startup.cs
@@ -62,7 +62,7 @@ namespace Promact.Trappist.Web
 
             services.AddMvc(config => { config.Filters.Add(typeof(GlobalExceptionFilter)); });
 
-            services.AddScoped<IQuestionsRespository, QuestionsRepository>();
+            services.AddScoped<IQuestionsRepository, QuestionsRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ITestsRepository, TestsRepository>();
             services.AddScoped<IStringConstants, StringConstants>();
