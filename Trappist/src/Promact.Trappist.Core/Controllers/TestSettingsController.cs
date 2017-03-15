d30562b0ac253a0256a7cf383bc4188eb401acac
madhurima <madhurima@PR2C2R6Y2016>
2017-03-15 08:59:29 +0530

    Created Server Side API for Test Settings.

---
+++ Trappist/src/Promact.Trappist.Core/Controllers/TestSettingsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Promact.Trappist.DomainModel.Models.Test;
+using Promact.Trappist.Repository.TestSettings;
+
+namespace Promact.Trappist.Core.Controllers
+{
+    public class TestSettingsController : Controller
+    {
+        private readonly ITestSettingsRepository _settingsRepository;
+
+        public TestSettingsController(ITestSettingsRepository settingsRepository)
+        {
+            _settingsRepository = settingsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetTestSettings()
+        {
+            var settings = _settingsRepository.GetTestSettings();
+            return Json(settings);
+        }
+
+        [HttpPut("api/put/{id}")]
+        public IActionResult UpdateTestSettings([FromRoute] int id, [FromBody] Test testObject)
+        {
+            _settingsRepository.UpdateTestSettings(id, testObject);
+            return Json(testObject);
+        }
+    }
+}
---
+++ Trappist/src/Promact.Trappist.Repository/TestSettings/TestSettingsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.Models.Test;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Promact.Trappist.Repository.TestSettings
+{
+    public class TestSettingsRepository : ITestSettingsRepository
+    {
+        private readonly TrappistDbContext _dbContext;
+        Test test = new Test();
+
+        public TestSettingsRepository(TrappistDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Updates the changes made to the Settings of a Test
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="testObject"></param>
+        /// <returns></returns>
+        public string UpdateTestSettings(int id, Test testObject)
+        {
+            testObject = new Test();
+            test = (from s in _dbContext.Test where s.Id == id select s).FirstOrDefault();
+            test = testObject;
+            _dbContext.Test.Update(test);
+            _dbContext.SaveChanges();
+            return "Data Updated";
+        }
+
+        /// <summary>
+        /// Get the Settings saved for a particular Test
+        /// </summary>
+        /// <returns>Settings set for that Test</returns>
+        public List<Test> GetTestSettings()
+        {
+            var settings = _dbContext.Test.ToList();
+            return settings;
+        }
+    }
+}
