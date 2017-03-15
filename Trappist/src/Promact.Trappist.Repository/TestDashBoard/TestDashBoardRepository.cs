002305f9839b77baceece833cae34a2880fffe04
asif <asif@PR2C2R5Y2015>
2017-03-15 12:51:15 +0530

    Created TestDashBoard Controller, TestDashBoardRepository,ITestDashBoardRepository, Server Side Api Implemented For TestDashBoard

---
+++ Trappist/src/Promact.Trappist.Repository/TestDashBoard/TestDashBoardRepository.cs
@@ -0,0 +1,36 @@
+﻿using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.Models.Test;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Promact.Trappist.Repository.TestDashBoard
+{
+    public class TestDashBoardRepository : ITestDashBoardRepository
+    {
+
+        private readonly TrappistDbContext _trappistdb;
+        public List<Test> Tests;
+
+        public TestDashBoardRepository(TrappistDbContext trappistdb)
+        {
+            _trappistdb = trappistdb;
+
+
+        }
+
+        /// <summary>
+        /// Fetch all the tests from Test Model,Convert it into List
+        /// </summary>
+        /// <returns>List of Tests</returns>
+        public List<Test> GetAllTests()
+        {
+
+             var Tests = _trappistdb.Test.ToList();
+             return Tests;
+
+        }
+
+
+    }
+}
