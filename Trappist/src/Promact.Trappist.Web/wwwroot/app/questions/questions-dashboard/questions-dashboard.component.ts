6f8759bd83e334a488f6319fbcbbe17fb9441b7a
partha <partha@PR2C6R4Y2016>
2017-03-16 19:35:00 +0530

    Client Side Edit Ui implemented Client side Api implememted Useless Method Deleted

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
@@ -1,11 +1,8 @@
-﻿
-import { Component, OnInit, ViewChild } from "@angular/core";
+﻿import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from "@angular/core";
 import { QuestionsService } from "../questions.service";
 import { CategoryService } from "../categories.service";
 import { MdDialog } from '@angular/material';
 import { Category } from "../category.model";
-import { Location } from '@angular/common';
-
 @Component({
     moduleId: module.id,
     selector: "questions-dashboard",
@@ -13,8 +10,8 @@ import { Location } from '@angular/common';
 })
 
 export class QuestionsDashboardComponent implements OnInit {
-    private category: Category = new Category();
-    categoryArray: string[] = new Array<string>();
+    category: Category = new Category();
+    categoryArray: Category[] = new Array<Category>();
 
     constructor(private questionsService: QuestionsService, private dialog: MdDialog, private categoryService: CategoryService) {
 
@@ -30,18 +27,20 @@ export class QuestionsDashboardComponent implements OnInit {
             this.categoryArray = CategoriesList;
         });
     }
-
     getAllQuestions() {
         this.questionsService.getQuestions().subscribe((questionsList) => {
-            console.log(questionsList);
+            //console.log(questionsList);
         });
     }
-
     // Open Add Category Dialog
     addCategoryDialog() {
         this.dialog.open(AddCategoryDialogComponent);
     }
-
+    //open Edit Category Dialog
+    editCategoryDialog(cat: any) {
+        var prop = this.dialog.open(EditCategoryDialogComponent).componentInstance;
+        prop.category = JSON.parse(JSON.stringify(cat));
+    }
 }
 
 @Component({
@@ -56,16 +55,16 @@ export class AddCategoryDialogComponent {
     }
 
     /*To a Category
-    *Add category In Cateogry Model
+    *Add category in Cateogry Model
     */
     CategoryAdd(category: Category) {
         this.categoryService.addCategory(category).subscribe((response) => {
-                this.dialog.closeAll();
+            this.dialog.closeAll();
         });
     }
 
     /*To check Whether CategoryName Exists or not
-    *If Exits it will return true and button will be disabled
+    *If categoryName Exists it will return true and button will be disabled
     */
     CheckDuplicateCategoryName(categoryName: string)
     {
@@ -75,5 +74,40 @@ export class AddCategoryDialogComponent {
     }
 
 }
+
+@Component({
+    moduleId: module.id,
+    selector: 'edit-category-dialog',
+    templateUrl: "edit-category-dialog.html"
+})
+
+export class EditCategoryDialogComponent {
+    category: Category = new Category();
+    isNameExist: boolean = false;
+    constructor(private categoryService: CategoryService, private dialog: MdDialog) {
+        }
+    
+    /*edit Category
+    *input : category 
+    */
+    categoryedit(category: Category) {
+        this.categoryService.editCategory(category.id, category).subscribe((response) => {
+            this.dialog.closeAll();
+        });
+    }
+
+    /* to check Whether CategoryName Exists or not
+    *If categoryName Exists it will return true and button will be disabled
+    */
+    CheckDuplicateCategoryName(categoryName: string) {
+        this.categoryService.checkDuplicateCategoryName(categoryName).subscribe((result) => {
+            this.isNameExist = result;
+        });
+    }
+}
+
+
+
+
 
 
