760892cb05f2006a341cb65b6799f9f0f1c6a3a6
asif-khan17 <asif@promactinfo.com>
2017-03-16 11:30:12 +0530

    Update TestDashBoardRepository.cs
    unneccessery things removed

--- Trappist/src/Promact.Trappist.Repository/TestDashBoard/TestDashBoardRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/TestDashBoard/TestDashBoardRepository.cs
@@ -19,8 +19,7 @@ namespace Promact.Trappist.Repository.TestDashBoard
         /// </summary>
         /// <returns>List of Tests</returns>
         public List<Test> GetAllTests()
-        {
-                      
+        {                      
             var Tests = _trappistdb.Test.ToList();
             return Tests;
 
