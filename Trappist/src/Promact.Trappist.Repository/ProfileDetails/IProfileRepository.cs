7974bbe93a4a6df2143281dfde5c2a96d57da183
ritika <ritika@promactinfo.com>
2017-03-16 12:11:42 +0530

    Added ProfileDetails Controller,IProfileRepository & ProfileRepository, Changed  profile-dashboard.component.ts, profile-dashboard.html,profile-edit.component.ts,profile-edit.html,Injected service in startup.cs

---
+++ Trappist/src/Promact.Trappist.Repository/ProfileDetails/IProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Promact.Trappist.Web.Models;
+
+namespace Promact.Trappist.Repository.ProfileDetails
+{
+    public interface IProfileRepository
+    {
+
+    /// <summary>
+    /// Updates current user's details
+    /// </summary>
+    /// <param name="updateUserDetails"></param>
+    /// <returns>Update user details in database</returns>
+    void UpdateProfile(ApplicationUser updateUserDetails);
+    }
+}
