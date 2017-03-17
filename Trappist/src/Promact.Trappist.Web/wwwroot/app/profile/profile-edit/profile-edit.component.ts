1c8dc2eaf02850c5acff92fd80287f454e0eb16a
ritika <ritika@promactinfo.com>
2017-03-17 13:06:55 +0530

    changes in profile-edit.component.ts

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/profile/profile-edit/profile-edit.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/profile/profile-edit/profile-edit.component.ts
@@ -1,18 +1,20 @@
 ﻿import { Component, OnInit, ViewChild } from "@angular/core";
 import { Http } from "@angular/http";
+import { MdSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 import { ApplicationUser } from "../profile.model";
 import { ProfileService } from "../profile.service";
 
 @Component({
-    moduleId: module.id,
-    selector: "profile-edit",
-    templateUrl: "profile-edit.html"
+  moduleId: module.id,
+  selector: "profile-edit",
+  templateUrl: "profile-edit.html"
 })
 
-export class ProfileEditComponent implements OnInit{
+export class ProfileEditComponent implements OnInit {
 
   editUser: ApplicationUser = new ApplicationUser();
-  constructor(public profileService:ProfileService ) { }
+  constructor(public profileService: ProfileService, private router: Router, public snackBar: MdSnackBar) { }
 
   ngOnInit() {
     this.editUserDetails();
@@ -25,7 +27,7 @@ export class ProfileEditComponent implements OnInit{
     this.profileService.editUserDetails().subscribe((response) => {
       this.editUser = response;
     });
-    }
+  }
 
   /**
   * update the  details of the user
@@ -34,6 +36,15 @@ export class ProfileEditComponent implements OnInit{
     this.profileService.updateUserDetails(this.editUser).subscribe((response) => {
 
     });
+
+    // Open Snackbar
+    let snackBarRef = this.snackBar.open('Saved Changes Successfully', 'Dismiss', {
+      duration: 3000,
+    });
+    snackBarRef.afterDismissed().subscribe(() => {
+      this.router.navigate(['/profile']);
+    });
+
   }
 
 }
