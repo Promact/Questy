d78552c6b780eeba6684812c674eeef5bb436868
Roshni Shah <shah.roshni187@gmail.com>
2017-03-17 10:27:16 +0530

    updated unit tests to remove business logic and added seed data

--- Trappist/src/Promact.Trappist.Test/ClearDatabase.cs
+++ Trappist/src/Promact.Trappist.Test/ClearDatabase.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.Enum;
+using Promact.Trappist.DomainModel.Models.Question;
 
 namespace Promact.Trappist.Test
 {
@@ -10,6 +12,19 @@ namespace Promact.Trappist.Test
         {
             dbContext.Database.EnsureDeleted();
             //seed data will go here.
+            var Languages = new CodingLanguage[]
+            {
+                new CodingLanguage {Language = ProgramingLanguage.Java},
+                new CodingLanguage {Language = ProgramingLanguage.Cpp},
+                new CodingLanguage {Language = ProgramingLanguage.C}
+            };
+
+            foreach (CodingLanguage language in Languages)
+            {
+                dbContext.CodingLanguage.Add(language);
+            }
+
+            dbContext.SaveChanges();
         }
     }
 }
