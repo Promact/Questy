25fd1b892b39a68441e4ef55338056cc270f6353
Roshni Shah <shah.roshni187@gmail.com>
2017-03-17 09:55:13 +0530

    Unit testing bootstrap with xUnit and Collection Fixtures

---
+++ Trappist/src/Promact.Trappist.Test/ClearDatabase.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Promact.Trappist.DomainModel.DbContext;
+
+namespace Promact.Trappist.Test
+{
+    public class ClearDatabase
+    {
+        public static void ClearDatabaseAndSeed(TrappistDbContext dbContext)
+        {
+            dbContext.Database.EnsureDeleted();
+            //seed data will go here.
+        }
+    }
+}
