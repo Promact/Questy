35ee31c99c5c2a7264abcbd45ed2472481a8090b
rushi <rushi_216@yahoo.com>
2017-03-17 11:13:35 +0530

    converted all projects to newer csproj format, updated travis config, updated packages

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM microsoft/aspnetcore:1.0
+FROM microsoft/aspnetcore:1.1
 WORKDIR /app
 COPY Trappist/src/Promact.Trappist.Web/published  ./
 ENTRYPOINT ["dotnet","Promact.Trappist.Web.dll"]
--- Trappist/src/Promact.Trappist.Web/Startup.cs
+++ Trappist/src/Promact.Trappist.Web/Startup.cs
@@ -38,9 +38,7 @@ namespace Promact.Trappist.Web
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
+                builder.AddUserSecrets<Startup>();
             }
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
@@ -49,8 +47,6 @@ namespace Promact.Trappist.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
             services.AddDbContext<TrappistDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly("Promact.Trappist.Web")));
             services.AddIdentity<ApplicationUser, IdentityRole>()
@@ -74,23 +70,31 @@ namespace Promact.Trappist.Web
             loggerFactory.AddDebug();
             loggerFactory.AddNLog();
             app.AddNLogWeb();
-            app.UseApplicationInsightsRequestTelemetry();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
-            /*else
+
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
-            }*/
-            app.UseApplicationInsightsExceptionTelemetry();
+            }
+
+
             app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions
+
+
+            if (env.IsDevelopment())
             {
-                FileProvider = new PhysicalFileProvider(Path.GetFullPath(Path.Combine(env.ContentRootPath, "node_modules"))),
-                RequestPath = new PathString("/node_modules")
-            });
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.GetFullPath(Path.Combine(env.ContentRootPath, "node_modules"))),
+                    RequestPath = new PathString("/node_modules")
+                });
+            }
+
             app.UseIdentity();
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
             app.UseMvc(routes =>
