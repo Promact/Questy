666b9394f9f02090eefeada6202f5af03a229f69
ritika <ritika@promactinfo.com>
2017-03-17 09:03:34 +0530

    Cahnges in ProfileDetailsController,IProfileRepository,ProfileRepositoy,profile-dashboard.component.ts,profile-edit.component.ts,profile.module.ts and added profile.model.ts and profile.service.ts

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/profile/profile-dashboard/profile-dashboard.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/profile/profile-dashboard/profile-dashboard.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, OnInit, ViewChild } from "@angular/core";
 import { MdDialog } from '@angular/material';
-import { Http } from '@angular/http';
+import { ApplicationUser } from "../profile.model";
+import { ProfileService } from "../profile.service";
 
 @Component({
     moduleId: module.id,
@@ -11,18 +12,18 @@ export class ProfileDashboardComponent implements OnInit {
 
   user: ApplicationUser = new ApplicationUser;
 
-
-
   ngOnInit() {
     this.GetUserDetails();
   }
 
-  constructor(public http: Http, public dialog: MdDialog) { }
+  constructor(public profileService:ProfileService, public dialog: MdDialog) { }
 
-  // Get's current user's details from the server and displays it
+  /**
+  * get details of the user
+  */
   GetUserDetails() {
-    this.http.get("api/Profile").subscribe((response) => {
-      this.user = response.json();
+    this.profileService.GetUserDetails().subscribe((response) => {
+      this.user = response;
     });
   }
 
@@ -32,15 +33,6 @@ export class ProfileDashboardComponent implements OnInit {
     }
    
 }
-
-class ApplicationUser {
-  Name: string;
-  OrganizationName: string;
-  UserName: string;
-  PhoneNumber: string;
-}
-
-
 
 @Component({
     moduleId: module.id,
