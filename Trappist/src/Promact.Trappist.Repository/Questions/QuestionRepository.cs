33ad979f92c9a88234f57e4d897ac8f257182e70
deepankar <deepankar@promactinfo.com>
2017-03-17 14:17:11 +0530

    Added API to handle post request to add new code snippet question

--- Trappist/src/Promact.Trappist.Repository/Questions/QuestionRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Questions/QuestionRepository.cs
@@ -2,12 +2,15 @@
 using Promact.Trappist.DomainModel.Models.Question;
 using System.Linq;
 using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.ApplicationClasses.Question;
+using AutoMapper;
 
 namespace Promact.Trappist.Repository.Questions
 {
     public class QuestionRepository : IQuestionRespository
     {
         private readonly TrappistDbContext _dbContext;
+
         public QuestionRepository(TrappistDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -30,14 +33,45 @@ namespace Promact.Trappist.Repository.Questions
         public void AddSingleMultipleAnswerQuestion(SingleMultipleAnswerQuestion singleMultipleAnswerQuestion, List<SingleMultipleAnswerQuestionOption> singleMultipleAnswerQuestionOption)
         {
             _dbContext.SingleMultipleAnswerQuestion.Add(singleMultipleAnswerQuestion);
-            foreach(SingleMultipleAnswerQuestionOption singleMultipleAnswerQuestionOptionElement in singleMultipleAnswerQuestionOption)
+            foreach (SingleMultipleAnswerQuestionOption singleMultipleAnswerQuestionOptionElement in singleMultipleAnswerQuestionOption)
             {
                 singleMultipleAnswerQuestionOptionElement.SingleMultipleAnswerQuestionID = singleMultipleAnswerQuestion.Id;
                 _dbContext.SingleMultipleAnswerQuestionOption.Add(singleMultipleAnswerQuestionOptionElement);
-            }   
+            }
             _dbContext.SaveChanges();
         }
-   
-   
+
+        /// <summary>
+        /// Add new code snippet question to the database
+        /// </summary>
+        /// <param name="codeSnippetQuestion">Code Snippet Question Model</param>
+        public void AddCodeSnippetQuestion(CodeSnippetQuestionDto codeSnippetQuestionModel)
+        {
+
+            CodeSnippetQuestion codeSnippetQuestion = Mapper.Map<CodeSnippetQuestionDto, CodeSnippetQuestion>(codeSnippetQuestionModel);
+
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                var question = _dbContext.CodeSnippetQuestion.Add(codeSnippetQuestion);
+                _dbContext.SaveChanges();
+
+                var codingLanguageList = codeSnippetQuestionModel.LanguageList;
+                var questionId = question.Entity.Id;
+
+                foreach (var language in codingLanguageList)
+                {
+                    var languageId = _dbContext.CodingLanguage.Where(x => x.Language == language).Select(x => x.Id).FirstOrDefault();
+                    _dbContext.QuestionLanguageMapping.Add(new QuestionLanguageMapping
+                    {
+                        QuestionId = questionId,
+                        LanguageId = languageId
+                    });
+                }
+                _dbContext.SaveChanges();
+
+                transaction.Commit();
+            }
+        }
+
     }
 }
