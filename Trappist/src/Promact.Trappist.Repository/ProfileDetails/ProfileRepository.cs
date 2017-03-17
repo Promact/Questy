0b376ee111a4d244d8c4680a2b897010dcca1798
ritika <ritika@promactinfo.com>
2017-03-17 09:03:34 +0530

    Cahnges in ProfileDetailsController,IProfileRepository,ProfileRepositoy,profile-dashboard.component.ts,profile-edit.component.ts,profile.module.ts and added profile.model.ts and profile.service.ts

--- Trappist/src/Promact.Trappist.Repository/ProfileDetails/ProfileRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/ProfileDetails/ProfileRepository.cs
@@ -1,15 +1,26 @@
 ﻿using Promact.Trappist.DomainModel.DbContext;
 using Promact.Trappist.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace Promact.Trappist.Repository.ProfileDetails
 {
-    public class ProfileRepository :IProfileRepository
+  public class ProfileRepository :IProfileRepository
     {
     private readonly TrappistDbContext _dbContext;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public ProfileRepository(TrappistDbContext dbContext)
+    public ProfileRepository(TrappistDbContext dbContext, UserManager<ApplicationUser> userManager)
     {
       _dbContext = dbContext;
+      _userManager = userManager;
+    }
+
+
+    public async Task<ApplicationUser> GetUserDetails(string name)
+    {
+      var user = await _userManager.FindByNameAsync(name);
+      return user;
     }
 
     /// <summary>
@@ -22,6 +33,7 @@ namespace Promact.Trappist.Repository.ProfileDetails
       var user = _dbContext.Users.Update(updateUserDetails);
       _dbContext.SaveChanges();
     }
+
 
   }
 }
