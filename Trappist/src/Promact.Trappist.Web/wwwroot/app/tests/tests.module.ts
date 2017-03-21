71bc7e016db3a391c2b2df81607f419aeb6d7b47
Shreel Desai <shreel@promactinfo.com>
2017-03-21 17:12:04 +0530

    Designed test-sections page.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.module.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.module.ts
@@ -7,6 +7,7 @@ import { TestCreateDialogComponent } from "./tests-dashboard/test-create-dialog.
 import { DeleteTestDialogComponent } from "./tests-dashboard/delete-test-dialog.component";
 import { TestSettingsComponent } from "./test-settings/test-settings.component";
 import { TestLaunchDialogComponent } from "./test-settings/test-launch-dialog.component";
+import { TestSectionsComponent } from './test-sections/test-sections.component';
 import { TestService } from "./tests.service";
 import { FilterPipe } from "./tests-dashboard/test-dashboard.pipe";
 import { TestSettingService } from "./testsetting.service";
@@ -25,6 +26,7 @@ import { ClipboardModule } from 'ngx-clipboard';
         DeleteTestDialogComponent,
         TestSettingsComponent,
         TestLaunchDialogComponent,
+        TestSectionsComponent,
         FilterPipe
 
     ],
