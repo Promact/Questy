8bac501f1a8c0909710d5ea79fe37a0e693145af
madhurima <madhurima@PR2C2R6Y2016>
2017-03-21 14:15:57 +0530

    Added validations for the fields of Test Settings Page and Performed Unit Testing for Test Settings.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/test-settings/test-settings.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/test-settings/test-settings.component.ts
@@ -3,6 +3,8 @@ import { MdDialog } from '@angular/material';
 import { Test } from "../tests.model";
 import { TestSettingService } from "../testsetting.service";
 import { ActivatedRoute } from '@angular/router';
+import { TestLaunchDialogComponent } from "./test-launch-dialog.component";
+
 
 
 @Component({
@@ -11,7 +13,7 @@ import { ActivatedRoute } from '@angular/router';
     templateUrl: "test-settings.html"
 })
 
-export class TestSettingsComponent{
+export class TestSettingsComponent {
     testsettings: Test = new Test();
     testId: number;
 
@@ -25,7 +27,7 @@ export class TestSettingsComponent{
     }
 
     //Gets the Settings saved for a particular Test
-    getTestSettings(id:number) {
+    getTestSettings(id: number) {
         this.testSettingService.getSettings(id).subscribe((response) => {
             this.testsettings = (response);
         });
@@ -34,31 +36,20 @@ export class TestSettingsComponent{
     //Selects the Test Name from the text box containing it on focus 
     selectAllContent($event: any) {
         $event.target.select();
-    }   
+    }
 
     //Updates the Edited Test Name in the Database on removing focus
     onBlurMethod(id: number, testObject: Test) {
-        this.testSettingService.updateSettings(id, testObject).subscribe((response) => {       
+        this.testSettingService.updateSettings(id, testObject).subscribe((response) => {
         });
     }
 
     //Launches the Test Dialog Box and also updates the Settings edited for the selected Test
     launchTestDialog(id: number, testObject: Test) {
-        this.testSettingService.updateSettings(id, testObject).subscribe((response) => {         
+        this.testSettingService.updateSettings(id, testObject).subscribe((response) => {
         });
         var instance = this.dialog.open(TestLaunchDialogComponent).componentInstance;
         instance.settingObject = testObject;
     }
 }
 
-@Component({
-    moduleId: module.id,
-    selector: 'test-launch-dialog',
-    templateUrl: "test-launch-dialog.html"
-})
-
-export class TestLaunchDialogComponent {
-    copiedContent: boolean = true;
-    settingObject: Test = new Test();
-    isDisplay: string = this.settingObject.link;
-}
