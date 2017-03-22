b121ac29cbb51a747716ebf9a0f711c23aac41a7
Shreel Desai <shreel@promactinfo.com>
2017-03-22 12:31:21 +0530

    Space formatting done.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/test-sections/test-sections.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/test-sections/test-sections.component.ts
@@ -1,11 +1,11 @@
 ﻿import { Component } from "@angular/core";
 
 @Component({
-    moduleId: module.id,
-    selector: "test-sections",
-    templateUrl: "test-sections.html"
+  moduleId: module.id,
+  selector: "test-sections",
+  templateUrl: "test-sections.html"
 })
 
-export class TestSectionsComponent{
+export class TestSectionsComponent {
 
 }
