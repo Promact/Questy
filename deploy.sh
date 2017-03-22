3bbe8ec7a87558bd808e78ccffc2910eb1bb6493
Rushi Soni <rushi.2106@gmail.com>
2017-03-22 17:26:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 cd ./Trappist/src/Promact.Trappist.Web
 dotnet ef migrations add prod
 dotnet publish -o published
+ls published/wwwroot
 cd ../../../
 docker build -t promact/trappist:$TRAVIS_BRANCH .
 docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
