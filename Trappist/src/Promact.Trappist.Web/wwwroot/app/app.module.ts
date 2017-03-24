d4608fc397b21a8b7a111de40853fbb4866fdf3e
rushi <rushi_216@yahoo.com>
2017-03-24 17:16:58 +0530

    made app files consistent with single quote strings

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/app.module.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/app.module.ts
@@ -1,17 +1,17 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 import 'rxjs/Rx';
 
-import { appRouting } from "./app.routing";
+import { appRouting } from './app.routing';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from "./shared/shared.module";
-import { CoreModule } from "./core/core.module";
-import { QuestionsModule } from "./questions/questions.module";
-import { TestsModule } from "./tests/tests.module";
-import { ProfileModule } from "./profile/profile.module";
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { QuestionsModule } from './questions/questions.module';
+import { TestsModule } from './tests/tests.module';
+import { ProfileModule } from './profile/profile.module';
 
 @NgModule({
     bootstrap: [AppComponent],
