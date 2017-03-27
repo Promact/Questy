773bedfa12c2ead2ac409798eb3d397f95866d65
asif <asif@PR2C2R5Y2015>
2017-03-27 10:22:42 +0530

    Regex defined for Test Name

--- Trappist/src/Promact.Trappist.DomainModel/Models/Test/Test.cs
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Test/Test.cs
@@ -7,6 +7,7 @@ namespace Promact.Trappist.DomainModel.Models.Test
     {
         [Required] 
         [MaxLength(150, ErrorMessage = "Test Name length Should be less than 150")] 
+        //Test Name Should be alpha-numeric and should accept some special characters as defined
         [RegularExpression("^[a - zA - Z0 - 9_@ $#%&*^{}+;:<>()-]*$",ErrorMessage ="Enter A Valid Test Name")]
         public string TestName { get; set; }
         public string Link { get; set; }
