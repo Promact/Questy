3b9e355b75867d1e7c61e7109d7a995e2f5ce0c6
Niharika Dubey <niharika@promactinfo.com>
2017-03-27 09:30:07 +0530

    client side test creation with Tslint issue resolved

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-create-dialog.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-create-dialog.component.ts
@@ -1,8 +1,46 @@
 ﻿import { Component } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { Test } from '../tests.model';
+import { TestService } from '../tests.service';
+import { Response } from '../tests.model';
+import { TestsDashboardComponent } from './tests-dashboard.component';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
     moduleId: module.id,
     selector: 'test-create-dialog',
     templateUrl: 'test-create-dialog.html'
 })
-export class TestCreateDialogComponent { }
+
+export class TestCreateDialogComponent {
+    test: Test = new Test();
+    responseObj: Response = new Response;
+    errorMessage: boolean = false;
+    testNameReference: string;
+    constructor(public dialogRef: MdDialogRef<TestCreateDialogComponent>, private testService: TestService, public snackBar: MdSnackBar) {
+    }
+    /**
+     * this method is used to add a new test
+     * @param testNameRef is name of the test
+     */
+    AddTest(testNameRef: string) {
+        this.test.testName = testNameRef;
+        this.testService.getTest(this.test.testName).subscribe((response) => {
+            this.responseObj = response;
+            console.log(response);
+            if (this.responseObj.responseValue) {
+                this.testService.addTests('api/tests', this.test).subscribe((responses) => {
+                    this.dialogRef.close(responses);
+                });
+            }
+            else
+                this.errorMessage = true;
+        });
+
+    }
+
+    //this method is used to disable the errorMessage
+    changeError() {
+        this.errorMessage = false;
+    }
+}
--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.service.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.service.ts
@@ -8,14 +8,26 @@ export class TestService {
     private testsApiUrl = 'api/tests';
 
     constructor(private httpService: HttpService) {
-
     }
-
     /**
      * get list of tests
      */
     getTests() {
-        return this.httpService.get(this.testsApiUrl);
+        return this.httpService.get(this.testApiUrl);
     }
-
+    /**
+     * add new test
+     * @param url
+     * @param test
+     */
+    addTests(url: string, test: any) {
+        return this.httpService.post(url, test);
+    }
+    /**
+     * get response whether test name is unique or not
+     * @param testName is name of the test
+     */
+    getTest(testName: string) {
+        return this.httpService.get(this.testApiUrl + '/' + testName);
+    }
 }
