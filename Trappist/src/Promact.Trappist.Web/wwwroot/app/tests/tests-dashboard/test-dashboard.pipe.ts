98151ae364728beb3fd760f6a2c03d0542d7004a
asif <asif@PR2C2R5Y2015>
2017-03-29 09:09:28 +0530

    Naming Convention Corrected

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.pipe.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.pipe.ts
@@ -1,17 +1,18 @@
 ﻿import { Pipe, PipeTransform } from '@angular/core';
+
 @Pipe({
-
     name: 'filter'
-
 })
 export class FilterPipe implements PipeTransform {
 
-    transform(value: any, term: any): any {
-        if (term === undefined)
-            return value;
-        return value.filter(function (x: any) {
-
-            return (x.testName.toLowerCase().includes(term.toLowerCase()) || x.link.toLowerCase().includes(term.toLowerCase()));
+    transform(allTests: any, searchedTest: any): any {
+        if (searchedTest === undefined)
+            return allTests;
+        return allTests.filter(function (currentTest: any) {
+            if (currentTest.link === null)
+                return (currentTest.testName.toLowerCase().includes(searchedTest.toLowerCase()))
+            else
+            return (currentTest.testName.toLowerCase().includes(searchedTest.toLowerCase()) || currentTest.link.toLowerCase().includes(searchedTest.toLowerCase()));
         });
     }
 }
