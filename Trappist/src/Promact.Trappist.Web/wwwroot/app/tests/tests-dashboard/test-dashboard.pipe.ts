b67cda0c8a7003e66903519251878e44c3f58197
asif <asif@PR2C2R5Y2015>
2017-03-30 16:00:37 +0530

    Changes made Accordingly..

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.pipe.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.pipe.ts
@@ -6,13 +6,15 @@ import { Test } from '../tests.model';
 })
 export class FilterPipe implements PipeTransform {
 
-    transform(allTests: Test[], searchedTest: string) : any {
-        if (searchedTest === undefined)
+    transform(allTests: Test[], searchedTest: string): any {
+
+        if (!searchedTest || !searchedTest.trim())
             return allTests;
+        console.log(searchedTest);
         return allTests.filter(function (currentTest: Test) {
             let searchedTestLower = searchedTest.toLowerCase();
             let currentTestName = currentTest.testName.toLowerCase();
-            if (currentTest.link === null)
+            if (!currentTest.link)
                 return (currentTestName.includes(searchedTestLower));
             else
                 return (currentTestName.includes(searchedTestLower) || currentTest.link.toLowerCase().includes(searchedTestLower));
