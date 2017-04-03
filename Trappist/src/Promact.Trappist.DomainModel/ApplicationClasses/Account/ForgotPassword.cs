cadceaa0a40c014485916732f2bcbd335642cfa7
Suaprna Acharya <suparna@promactinfo.com>
2017-04-03 10:35:15 +0530

    changed validation messages for password and email

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/ForgotPassword.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/ForgotPassword.cs
@@ -5,7 +5,7 @@ namespace Promact.Trappist.DomainModel.ApplicationClasses.Account
     public class ForgotPassword
     {
         [Required]
-        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Invalid Email Format!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
     }
 }
--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Account/Login.cs
@@ -5,11 +5,11 @@ namespace Promact.Trappist.DomainModel.ApplicationClasses.Account
     public class Login
     {
         [Required]
-        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Invalid Email Format!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(14, ErrorMessage = "Invalid Password Format! It must have at least {2} and at max {1} characters", MinimumLength = 8)]
+        [StringLength(14, ErrorMessage = "Password must be alphanumeric including at least 1 uppercase letter and a special character with 8 to 14 characters", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
