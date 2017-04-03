e648eb3eb16d4d03315df56d0e9ce39375ad0709
Shreel Bhatt <shreel@promactinfo.com>
2017-04-03 11:51:56 +0530

    Fixed the routing for test conduct module.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/conduct.routing.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/conduct.routing.ts
@@ -1,11 +1,16 @@
 ﻿import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 const conductRoutes: Routes = [
     {
-        path: '',
-        redirectTo: '/register',
-        pathMatch: 'full'
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'instructions',
+        component: InstructionsComponent
     }
 ];
 
---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/instructions/instructions.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'instructions',
+    templateUrl: 'instructions.html',
+})
+export class InstructionsComponent { }
