27fec3bbf2980681a3c59591cdc2b966b726821e
deepankar <deepankar@promactinfo.com>
2017-04-03 08:46:16 +0530

    Added unit testing for AddCodeSnippetQuestion function
    Added required scope in bootstrap file

--- Trappist/src/Promact.Trappist.Test/Bootstrap.cs
+++ Trappist/src/Promact.Trappist.Test/Bootstrap.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using Promact.Trappist.DomainModel.ApplicationClasses.BasicSetup;
+using Promact.Trappist.DomainModel.ApplicationClasses.Question;
 using Promact.Trappist.DomainModel.DbContext;
-using Promact.Trappist.Repository.Questions;
+using Promact.Trappist.DomainModel.Models.Question;
+using Promact.Trappist.Repository.BasicSetup;
 using Promact.Trappist.Repository.Categories;
+using Promact.Trappist.Repository.Profile;
+using Promact.Trappist.Repository.Questions;
 using Promact.Trappist.Repository.Tests;
 using Promact.Trappist.Utility.Constants;
 using Microsoft.EntityFrameworkCore;
@@ -11,8 +22,9 @@ using Promact.Trappist.DomainModel.ApplicationClasses.BasicSetup;
 using Promact.Trappist.Repository.Profile;
 using Promact.Trappist.Web.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
-using Promact.Trappist.Utility.GlobalUtil;
+using Promact.Trappist.Utility.EmailServices;
+using Promact.Trappist.Web.Models;
+using System;
 
 namespace Promact.Trappist.Test
 {
@@ -32,8 +44,9 @@ namespace Promact.Trappist.Test
                 AddDbContext<TrappistDbContext>((serviceProvider, options) =>
                 {
                     options.UseInMemoryDatabase()
-                           .UseInternalServiceProvider(serviceProvider);
-                },ServiceLifetime.Transient);
+                           .UseInternalServiceProvider(serviceProvider)
+                           .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                }, ServiceLifetime.Transient);
 
             //Register all dependencies here
             services.Configure<ConnectionString>(x => x.Value = "connectionstring");
@@ -50,12 +63,26 @@ namespace Promact.Trappist.Test
                 .AddEntityFrameworkStores<TrappistDbContext>()
                 .AddDefaultTokenProviders();
             services.AddScoped(config => config.GetService<IOptionsSnapshot<EmailSettings>>().Value);
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<TrappistDbContext>().AddDefaultTokenProviders();
             services.AddScoped<IQuestionRespository, QuestionRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ITestsRepository, TestsRepository>();
             services.AddScoped<IStringConstants, StringConstants>();
             services.AddScoped<IProfileRepository, ProfileRepository>();
-            services.AddScoped<IGlobalUtil, GlobalUtil>();
+            services.AddScoped<IBasicSetupRepository, BasicSetupRepository>();
+            services.AddScoped<IEmailService, EmailService>();
+            var hostingEnvironmentMock = new Mock<IHostingEnvironment>();
+            var environmenrMockObject = hostingEnvironmentMock.Object;
+            services.AddScoped(x => hostingEnvironmentMock);
+            services.AddScoped(x => environmenrMockObject);
+
+            #region Auto Mapper Configuration
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<CodeSnippetQuestionAC, CodeSnippetQuestion>();
+            });
+            #endregion
+
             return services.BuildServiceProvider();
         }
     }
--- Trappist/src/Promact.Trappist.Test/Questions/QuestionsRepositoryTest.cs
+++ Trappist/src/Promact.Trappist.Test/Questions/QuestionsRepositoryTest.cs
@@ -1,16 +1,134 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Promact.Trappist.DomainModel.ApplicationClasses.Question;
 using Promact.Trappist.DomainModel.DbContext;
+using Microsoft.Extensions.DependencyInjection;
+using Promact.Trappist.Repository.BasicSetup;
+using Promact.Trappist.Repository.Questions;
+using Promact.Trappist.Web.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
 
 namespace Promact.Trappist.Test.Questions
 {
     [Collection("Register Dependency")]
     public class QuestionsRepositoryTest : BaseTest
     {
-        public QuestionsRepositoryTest(Bootstrap bootstrap) : base(bootstrap)
-        {   
+        private readonly Bootstrap _bootstrap;
+        private readonly TrappistDbContext _trappistDbContext;
+        private readonly IBasicSetupRepository _basicSetupRepository;
+        private readonly IQuestionRespository _questionRepository;
+        private readonly Microsoft.AspNetCore.Identity.SignInManager<ApplicationUser> _signInManager;
+        private readonly Mock<IHostingEnvironment> _hostingEnvironmentMock;
 
+        public QuestionsRepositoryTest(Bootstrap bootstrap) : base(bootstrap)
+        {
+            _bootstrap = bootstrap;
+            //resolve dependency to be used in tests
+            _trappistDbContext = _bootstrap.ServiceProvider.GetService<TrappistDbContext>();
+            _basicSetupRepository = _bootstrap.ServiceProvider.GetService<IBasicSetupRepository>();
+            _questionRepository = _bootstrap.ServiceProvider.GetService<IQuestionRespository>();
+            _signInManager = _bootstrap.ServiceProvider.GetService<Microsoft.AspNetCore.Identity.SignInManager<ApplicationUser>>();
+            _hostingEnvironmentMock = bootstrap.ServiceProvider.GetService<Mock<IHostingEnvironment>>();
+            ClearDatabase.ClearDatabaseAndSeed(_trappistDbContext);
         }
-               
+
+        /// <summary>
+        /// Test to add new code snippet question
+        /// </summary>
+        [Fact]
+        public async Task AddCodeSnippetQuestionAsync()
+        {
+            _hostingEnvironmentMock.Setup(x => x.ContentRootPath).Returns(string.Empty);
+
+            var applicationUser = CreatApplicationUser();
+            var result = await _basicSetupRepository.CreateAdminUser(applicationUser);
+
+            var codingQuestion = await CreateCodingQuestion();
+            await _questionRepository.AddCodeSnippetQuestionAsync(codingQuestion);
+
+            Assert.True(_trappistDbContext.Question.Count(x => x.QuestionDetail == codingQuestion.Question.QuestionDetail) == 1);
+        }
+
+        /// <summary>
+        /// Creates Coding Question
+        /// </summary>
+        /// <returns>Created CodingQuestion object</returns>
+        private async Task<QuestionAC> CreateCodingQuestion()
+        {
+            var categoryToCreate = CreateCategory();
+            var category = await _trappistDbContext.Category.AddAsync(categoryToCreate);
+
+            QuestionAC codingQuestion = new QuestionAC
+            {
+                Question = new DomainModel.Models.Question.Question
+                {
+                    QuestionDetail = "<h1>Write a program to add two number</h1>",
+                    CategoryID = category.Entity.Id,
+                    DifficultyLevel = DomainModel.Enum.DifficultyLevel.Easy,
+                    QuestionType = DomainModel.Enum.QuestionType.Programming
+                },
+                CodeSnippetQuestionAC = new CodeSnippetQuestionAC
+                {
+                    CheckCodeComplexity = true,
+                    CheckTimeComplexity = true,
+                    RunBasicTestCase = true,
+                    RunCornerTestCase = false,
+                    RunNecessaryTestCase = false
+                },
+                SingleMultipleAnswerQuestionAC = null
+            };
+
+            return codingQuestion;
+        }
+
+        /// <summary>
+        /// Creates dummy category
+        /// </summary>
+        /// <returns>Created Category object</returns>
+        private DomainModel.Models.Category.Category CreateCategory()
+        {
+            var category = new DomainModel.Models.Category.Category
+            {
+                CategoryName = "Test Category",
+                CreatedDateTime = DateTime.UtcNow
+            };
+
+            return category;
+        }
+
+        /// <summary>
+        /// Creates dummy ApplicationUser
+        /// </summary>
+        /// <returns>Created ApplicationUser object</returns>
+        private DomainModel.ApplicationClasses.BasicSetup.BasicSetupModel CreatApplicationUser()
+        {
+            var applicationUser = new DomainModel.ApplicationClasses.BasicSetup.BasicSetupModel
+            {
+                ConnectionString = new DomainModel.ApplicationClasses.BasicSetup.ConnectionString
+                {
+                    Value = "Server = (localdb)\\mssqllocaldb; Database = Trappist; Trusted_Connection = True"
+                },
+                EmailSettings = new DomainModel.ApplicationClasses.BasicSetup.EmailSettings
+                {
+                    Server = "webmail.promactinfo.com",
+                    Port = 587,
+                    UserName = "support@promactinfo.com",
+                    Password = "Promact2016",
+                    ConnectionSecurityOption = "None"
+                },
+                RegistrationFields = new DomainModel.ApplicationClasses.BasicSetup.RegistrationFields
+                {
+                    Name = "Deepankar Sinha",
+                    Email = "deepankar@gmail.com",
+                    Password = "Abcd@1234",
+                    ConfirmPassword = "Abcd@1234"
+                }
+            };
+
+            return applicationUser;
+        }
     }
 }
