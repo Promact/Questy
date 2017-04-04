c0bde918edbbac89ac3fd747d924d03924ce56bb
Ritika Mohata <ritika.mohata05@gmail.com>
2017-04-04 11:00:48 +0530

    Added comments

--- Trappist/src/Promact.Trappist.Test/Profile/ProfileRepositoryTest.cs
+++ Trappist/src/Promact.Trappist.Test/Profile/ProfileRepositoryTest.cs
@@ -1,5 +1,4 @@
-﻿using Promact.Trappist.DomainModel.DbContext;
-using Promact.Trappist.Repository.Profile;
+﻿using Promact.Trappist.Repository.Profile;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using Microsoft.AspNetCore.Identity;
@@ -13,27 +12,19 @@ namespace Promact.Trappist.Test.Profile
     [Collection("Register Dependency")]
     public class ProfileRepositoryTest : BaseTest
     {
-        public ProfileRepositoryTest(Bootstrap bootstrap) : base(bootstrap)
-        {
-
-        }
-    public class ProfileRepositoryTest
-    {
-        private readonly Bootstrap _bootstrap;
-        private readonly TrappistDbContext _trappistDbContext;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IProfileRepository _profileRepository;
 
-        public ProfileRepositoryTest(Bootstrap bootstrap)
+        public ProfileRepositoryTest(Bootstrap bootstrap) : base(bootstrap)
         {
-            _bootstrap = bootstrap;
             //resolve dependency to be used in tests
-            _trappistDbContext = _bootstrap.ServiceProvider.GetService<TrappistDbContext>();
-            _profileRepository = _bootstrap.ServiceProvider.GetService<IProfileRepository>();
-            _userManager = _bootstrap.ServiceProvider.GetService<UserManager<ApplicationUser>>();
-            ClearDatabase.ClearDatabaseAndSeed(_trappistDbContext);
+            _profileRepository = _scope.ServiceProvider.GetService<IProfileRepository>();
+            _userManager = _scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
         }
 
+        /// <summary>
+        /// Test case to get the details of the user
+        /// </summary>
         [Fact]
         public async Task GetUserDetailsAsync()
         {
@@ -43,6 +34,9 @@ namespace Promact.Trappist.Test.Profile
             Assert.Equal(result, user);
         }
 
+        /// <summary>
+        /// Test case to update profile details of the user
+        /// </summary>
         [Fact]
         public async void UpdateProfile()
         {
@@ -59,6 +53,10 @@ namespace Promact.Trappist.Test.Profile
             Assert.True(_trappistDbContext.Users.Count(x => x.PhoneNumber == userToUpdate.PhoneNumber) == 1);
         }
 
+        /// <summary>
+        /// Test case to update new password of the user
+        /// </summary>
+        /// <returns>Returns true and updates the new password of the user</returns>
         [Fact]
         public async Task UpdateUserPasswordAsync()
         {
@@ -69,6 +67,10 @@ namespace Promact.Trappist.Test.Profile
             Assert.True(result);
         }
 
+        /// <summary>
+        /// test case to check invalid old password
+        /// </summary>
+        /// <returns>returns false and does not update the new password as old password is wrong</returns>
         [Fact]
         public async Task InvalidUpdateUserPasswordAsync()
         {
