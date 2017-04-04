e44a9891f63bfc6b7dab3861d5dfccf396478672
Shreel Bhatt <shreel@promactinfo.com>
2017-04-04 17:22:07 +0530

    Designed Test Summary page & End Test Message page.

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/test-end/test-end.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'test-end',
+    templateUrl: 'test-end.html',
+})
+export class TestEndComponent { }
---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/test-summary/test-summary.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'test-summary',
+    templateUrl: 'test-summary.html',
+})
+export class TestSummaryComponent { }
