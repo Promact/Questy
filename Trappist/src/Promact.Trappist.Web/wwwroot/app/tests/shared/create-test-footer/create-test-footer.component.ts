8a0628d52a8197b7a1ba8cde6b35836c489ae55c
Shreel Bhatt <shreel@promactinfo.com>
2017-04-07 15:04:16 +0530

    Made structural changes for create test module and test conduct module.

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/shared/create-test-footer/create-test-footer.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'create-test-footer',
+    templateUrl: 'create-test-footer.html',
+})
+export class CreateTestFooterComponent { }
---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/shared/create-test-header/create-test-header.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'create-test-header',
+    templateUrl: 'create-test-header.html',
+})
+export class CreateTestHeaderComponent { }
