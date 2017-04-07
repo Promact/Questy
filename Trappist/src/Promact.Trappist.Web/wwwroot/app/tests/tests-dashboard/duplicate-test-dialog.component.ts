f6e1e71980bd87973651cc71ffee25ba88d70e3d
Ritika Mohata <ritika.mohata05@gmail.com>
2017-04-07 17:21:33 +0530

    added duplicate test dialog

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/duplicate-test-dialog.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+import { Test } from '../tests.model';
+
+@Component({
+    moduleId: module.id,
+    selector: 'duplicate-test-dialog',
+    templateUrl: 'duplicate-test-dialog.html'
+})
+export class DuplicateTestDialogComponent {
+    testName: string;
+    testArray: Test[] = new Array<Test>();
+
+}
