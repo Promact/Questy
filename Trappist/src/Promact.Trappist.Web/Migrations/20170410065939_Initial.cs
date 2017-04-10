ab93fe3fef3d83403e828a90fe2d4a233e185fd2
Roshni Shah <roshni@promactinfo.com>
2017-04-10 12:30:23 +0530

    made migration name with Pascal casing

--- Trappist/src/Promact.Trappist.Web/Migrations/20170410065939_Initial.cs
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170410065939_Initial.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Promact.Trappist.Web.Migrations
 {
-    public partial class initial : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
