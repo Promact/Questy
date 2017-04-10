8a49e043d32059de22df76fc2fe09b39b296f57b
Niharika Dubey <niharika@promactinfo.com>
2017-04-10 18:14:02 +0530

    testcategory model created

--- Trappist/src/Promact.Trappist.DomainModel/Models/Test/TestCategory.cs
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Test/TestCategory.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Promact.Trappist.DomainModel.Models.Test
 {
-   public class TestCategory
+    public class TestCategory
     {
+        [Key]
+        int? Id { get; set; }
+        int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category.Category Category {get;set;}
+        int TestId { get; set; }
+        [ForeignKey("TestId")]
+        public virtual Test Test { get; set; }
     }
 }
