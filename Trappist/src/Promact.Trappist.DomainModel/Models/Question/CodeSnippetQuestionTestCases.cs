e8374c2e26522f2b2bbfd6742ded32aaafdce61e
sandipan <sandipan@PR2C1R6Y2016>
2017-04-11 15:25:44 +0530

    formatted as per suggestion

--- Trappist/src/Promact.Trappist.DomainModel/Models/Question/CodeSnippetQuestionTestCases.cs
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Question/CodeSnippetQuestionTestCases.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Promact.Trappist.DomainModel.Models.Question
 {
-    public class CodeSnippetQuestionTestCases:BaseModel
+    public class CodeSnippetQuestionTestCases : BaseModel
     {
         [Required]
         public string TestCaseTitle { get; set; }
@@ -15,7 +15,7 @@ namespace Promact.Trappist.DomainModel.Models.Question
 
         [Required]
         public string TestCaseInput { get; set; }
-        
+
         [Required]
         public string TestCaseOutput { get; set; }
 
@@ -23,6 +23,7 @@ namespace Promact.Trappist.DomainModel.Models.Question
         public double TestCaseMarks { get; set; }
 
         public int CodeSnippetQuestionId { get; set; }
+
         public virtual CodeSnippetQuestion CodeSnippetQuestion { get; set; }
     }
 }
