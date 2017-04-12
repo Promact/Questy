9d1e1ccb8db0fc4f953628edbc0c8a6382edc620
rushi <rushi_216@yahoo.com>
2017-04-12 16:46:55 +0530

    fixed baseUrl in systemjs

--- Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
+++ Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
@@ -4,10 +4,11 @@
  */
 (function (global) {
     System.config({
+        baseURL: '/',
         defaultJSExtensions: true,
         paths: {
             // paths serve as alias
-            'npm:': '/node_modules/'
+            'npm:': 'node_modules/'
         },
         // map tells the System loader where to look for things
         map: {
