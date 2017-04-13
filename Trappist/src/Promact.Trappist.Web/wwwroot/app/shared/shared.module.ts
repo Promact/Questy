e39166841e223a6458e7781908a374b8cfd38835
rushi <rushi_216@yahoo.com>
2017-04-13 11:22:13 +0530

    updated to angular 4

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/shared/shared.module.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/shared/shared.module.ts
@@ -2,6 +2,7 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { Md2AccordionModule } from 'md2';
 
@@ -9,6 +10,7 @@ import { Md2AccordionModule } from 'md2';
     imports: [
         CommonModule,
         FormsModule,
+        BrowserAnimationsModule,
         MaterialModule.forRoot(),
         RouterModule,
         Md2AccordionModule.forRoot()
--- Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
+++ Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
@@ -13,10 +13,13 @@
         // map tells the System loader where to look for things
         map: {
             // angular bundles
+            '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+            '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
@@ -41,8 +44,7 @@
                 defaultExtension: 'js'
             },
             'ngx-clipboard': {
-                main: 'dist/index.umd.js',
-                //  defaultExtension: 'js'
+                main: 'dist/bundles/ngxClipboard.umd.js'
             },
             'clipboard': {
                 defaultExtension: 'js'
