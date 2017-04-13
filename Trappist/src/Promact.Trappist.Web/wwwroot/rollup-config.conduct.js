d673d4149e70e54242164856ec26f6592052ea11
rushi <rushi_216@yahoo.com>
2017-04-13 13:59:25 +0530

    added aot production bundling

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/rollup-config.conduct.js
@@ -0,0 +1,29 @@
+﻿import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry: 'wwwroot/app/main.conduct.aot.js',
+    sourceMap: false,
+    format: 'iife',
+    onwarn: function (warning) {
+        // Skip certain warnings
+
+        // should intercept ... but doesn't in some rollup versions
+        if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+
+        // console.warn everything else
+        console.warn(warning.message);
+    },
+    plugins: [
+        nodeResolve({ jsnext: true, module: true }),
+        commonjs({
+            include: ['node_modules/**'],
+            
+            namedExports: {
+                'node_modules/clipboard/dist/clipboard.js': ['Clipboard']
+            }
+        }),
+        uglify()
+    ]
+}
