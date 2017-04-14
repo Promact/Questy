973307d10f55cb985d17b42400cd50aae84b2780
Roshni Shah <roshni@promactinfo.com>
2017-04-14 11:36:30 +0530

    alltrim extension to remove unwanted white spaces from string.

---
+++ Trappist/src/Promact.Trappist.Utility/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Promact.Trappist.Utility.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// This method will remove all leading, trailing 
+        /// and extra white spaces within the string. 
+        /// </summary>
+        /// <param name="value">string to trim spaces</param>
+        /// <returns>string after removing extra white spaces.</returns>
+        public static string AllTrim(this string value)
+        {
+            return Regex.Replace(value.Trim(), "\\s+", " ");
+        }
+    }
+
+}
