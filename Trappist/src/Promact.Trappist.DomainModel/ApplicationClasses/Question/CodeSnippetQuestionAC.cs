23c4922b99285d1acd79ad17f75d97fdb0489096
deepankar <deepankar@promactinfo.com>
2017-04-14 11:47:47 +0530

    Removed inheritance from CodeSnippetQuestionAC class. Removed unecessary code.

--- Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodeSnippetQuestionAC.cs
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CodeSnippetQuestionAC.cs
@@ -4,10 +4,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Promact.Trappist.DomainModel.ApplicationClasses.Question
 {
-    public class CodeSnippetQuestionAC : CodeSnippetQuestion
+    public class CodeSnippetQuestionAC
     {
+        public bool CheckCodeComplexity { get; set; }
+
+        public bool CheckTimeComplexity { get; set; }
+
+        public bool RunBasicTestCase { get; set; }
+
+        public bool RunCornerTestCase { get; set; }
+
+        public bool RunNecessaryTestCase { get; set; }
+
         [Required]
         public ICollection<string> LanguageList { get; set; }
+
         public ICollection<CodeSnippetQuestionTestCases> TestCases { get; set; }
     }
 }
