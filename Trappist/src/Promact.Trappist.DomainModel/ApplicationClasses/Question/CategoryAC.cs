8976dddabb55467302f746aab1d0827d80b024cf
Niharika Dubey <niharika@promactinfo.com>
2017-04-19 18:52:26 +0530

    server side implemetation with model additions and migration

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/Question/CategoryAC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.Question
+{
+    public class CategoryAC
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<QuestionAC> QuestionList { get; set; }
+        public bool IsSelect = false;
+    }
+}
