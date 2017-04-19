2ed5b91e6e9f37a09f82846fa529c244ac823863
asif <asif@PR2C2R5Y2015>
2017-04-19 17:21:50 +0530

    TestQuestion and  TestCategory models added

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Test/TestCategory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.Test
+{
+    public class TestCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category.Category Category { get; set; }
+
+        [ForeignKey("TestId")]
+        public virtual Test Test { get; set; }
+    }
+}
