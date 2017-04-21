fea82376f28085e612b72848fdd99ab0a7f310a7
asif <asif@PR2C2R5Y2015>
2017-04-21 15:08:10 +0530

    Comments added to TestRepository and controller, extra space removed, routes changed accordingly in test-questions.component

--- Trappist/src/Promact.Trappist.Repository/Test/TestsRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/Test/TestsRepository.cs
@@ -101,16 +101,18 @@ namespace Promact.Trappist.Repository.Tests
         #region Test-Question-Selection
         public async Task<List<QuestionAC>> GetAllTestCategoryQuestionsByIdAsync(int testId, int categoryId)
         {
-            QuestionAC questionAc = new QuestionAC();
-            List<QuestionAC> questionListAc = new List<QuestionAC>();
+            var questionAc = new QuestionAC();
+            var questionListAc = new List<QuestionAC>();
             var testQuestionList = await _dbContext.TestQuestion.Where(x => x.TestId == testId).ToListAsync();
+            //Fetches the list of questions from Question Model
             var questionList = await _dbContext.Question.Where(x => x.CategoryID == categoryId).Include(y => y.SingleMultipleAnswerQuestion).ThenInclude(x => x.SingleMultipleAnswerQuestionOption).ToListAsync();
-
+            //Maps the each question in Question Model to QuestionAC object and make list of type QuestionAC
             questionList.ForEach(question =>
             {
                 questionAc = new QuestionAC();
                 questionAc.Question = Mapper.Map<Question, QuestionDetailAC>(question);
                 questionAc.SingleMultipleAnswerQuestion = Mapper.Map<SingleMultipleAnswerQuestion, SingleMultipleAnswerQuestionAC>(question.SingleMultipleAnswerQuestion);
+                //Checks if the question is already exists in TestQuestion Model,if exists,its IsSelect property made true
                 if (testQuestionList.Exists(x => x.QuestionId == questionAc.Question.Id && x.TestId == testId))
                     questionAc.Question.IsSelect = true;
                 questionListAc.Add(questionAc);
@@ -121,49 +123,60 @@ namespace Promact.Trappist.Repository.Tests
         public async Task<string> AddTestQuestionsAsync(List<QuestionAC> questionsToAddTest, int testId)
         {
             bool isDeleted = false;
-            TestQuestion testQuestionObj = new TestQuestion();
-            List<TestQuestion> testQuestionList = new List<TestQuestion>();
+            var testQuestionList = new List<TestQuestion>();
+            //Adds each question to TestQuestion Model whose IsSelct property is true
             foreach (var questionToAdd in questionsToAddTest)
             {
                 var question = Mapper.Map<QuestionDetailAC, Question>(questionToAdd.Question);
+                //Checks if the question exists in TestQuestion 
                 var questionExistInTest = await _dbContext.TestQuestion.FirstOrDefaultAsync(x => x.QuestionId == question.Id);
+                //if question exists in TestQuestion and its IsSelect property is false,then that question will be deleted from TestQuestion
                 if (questionExistInTest != null && !questionToAdd.Question.IsSelect)
                 {
                     _dbContext.TestQuestion.Remove(questionExistInTest);
                     await _dbContext.SaveChangesAsync();
                     isDeleted = true;
                 }
-
+                //Checks if question's IsSelect property is true
                 else if (questionToAdd.Question.IsSelect)
                 {
-                    testQuestionObj = new TestQuestion();
+                    //Creates TestQuestion Object
+                    var testQuestionObj = new TestQuestion();
                     testQuestionObj.QuestionId = question.Id;
                     testQuestionObj.TestId = testId;
-
+                    //If question is already present in the same Test, it wont be added to TestQuestion 
                     if (!await _dbContext.TestQuestion.AnyAsync(x => x.QuestionId == question.Id && x.TestId == testId))
                         testQuestionList.Add(testQuestionObj);
                 }
             }
+            //Returns message that user has not selected any new question 
             if (!testQuestionList.Any() && !isDeleted)
                 return _stringConstants.NoNewChanges;
             else
             {
                 await _dbContext.TestQuestion.AddRangeAsync(testQuestionList);
                 await _dbContext.SaveChangesAsync();
+                //Returns success message 
                 return _stringConstants.SuccessfullySaved;
             }
         }
 
         public async Task<TestAC> GetTestDetailsByIdAsync(int testId)
         {
-            TestAC testAcObject = new TestAC();
+           
+            //Find the test by Id from Test Model
             var test = await _dbContext.Test.FindAsync(testId);
-            testAcObject = Mapper.Map<Test, TestAC>(test);
+            //Maps that test with TestAC
+            var testAcObject = Mapper.Map<Test, TestAC>(test);
+            //Fetches the category list from Category Model
             var categoryList = await _dbContext.Category.ToListAsync();
+            //Maps Category list to CategoryAC lis
             var categoryListAc = Mapper.Map<List<Category>, List<CategoryAC>>(categoryList);
+            //Fetches the list of Categories from TestCategory Model
             var testCategoryList = await _dbContext.TestCategory.Where(x => x.TestId == testId).Include(x => x.Category).ToListAsync();
             categoryListAc.ForEach(category =>
             {
+                //If category present in TestCategory Model,then its IsSelect property made true
                 if (testCategoryList.Exists(x => x.CategoryId == category.Id))
                     category.IsSelect = true;
             });
