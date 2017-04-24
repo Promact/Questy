8a6c6ad702a762a7365b257eed7b03311e4b67cb
partha <partha@PR2C6R4Y2016>
2017-04-24 14:56:46 +0530

    Bugsolved

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-dashboard/questions-dashboard.component.ts
@@ -165,8 +165,9 @@ export class QuestionsDashboardComponent implements OnInit {
         deleteDialogRef.afterClosed().subscribe(
             deletedQuestion => {
                 if (deletedQuestion) {
-                    this.question.slice(this.question.indexOf(deletedQuestion), 1);
-                    this.questionDisplay.splice(this.question.indexOf(deletedQuestion), 1);
+                    this.question.splice(this.question.indexOf(deletedQuestion), 1);
+                    this.questionDisplay.splice(this.questionDisplay.indexOf(deletedQuestion), 1);
+                    this.countQuestion();
                 }
             });
     }
