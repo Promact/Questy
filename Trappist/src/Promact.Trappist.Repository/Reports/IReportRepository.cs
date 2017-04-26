38da66b69540d997a3ff55837deeffe1a5e7aa52
Sandipan Ghosh <sandipan@promactinfo.com>
2017-04-26 08:14:52 +0530

    implemented displaying feature of multiple student report page.

---
+++ Trappist/src/Promact.Trappist.Repository/Reports/IReportRepository.cs
@@ -0,0 +1,16 @@
+﻿using Promact.Trappist.DomainModel.Models.TestConduct;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.Repository.Reports
+{
+    public interface IReportRepository
+    {
+        /// <summary>
+        /// Method to get the details of all the Test-Attendees
+        /// </summary>
+        /// <param name="id">Id of the respective Test</param>
+        /// <returns></returns>
+        Task<ICollection<TestAttendees>> GetAllTestAttendeesAsync(int id);
+    }
+}
