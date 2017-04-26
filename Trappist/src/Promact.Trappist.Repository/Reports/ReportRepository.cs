a6758e6f711a8548c7eac23b32dbaf5707c3712b
Sandipan Ghosh <sandipan@promactinfo.com>
2017-04-26 08:14:52 +0530

    implemented displaying feature of multiple student report page.

---
+++ Trappist/src/Promact.Trappist.Repository/Reports/ReportRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Promact.Trappist.DomainModel.DbContext;
+using Promact.Trappist.DomainModel.Models.TestConduct;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.Trappist.Repository.Reports
+{
+    public class ReportRepository : IReportRepository
+    {
+        private readonly TrappistDbContext _dbContext;
+
+        #region Constructor
+        public ReportRepository(TrappistDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        #endregion
+
+        public async Task<ICollection<TestAttendees>> GetAllTestAttendeesAsync(int id)
+        {
+           var result= await _dbContext.TestAttendees.Where(t => t.TestId == id).ToListAsync();
+            return result;
+        }
+    }
+}
--- Trappist/src/Promact.Trappist.Web/wwwroot/app/reports/test-report/test-report.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/reports/test-report/test-report.component.ts
@@ -1,4 +1,7 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../report.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TestAttendee } from '../testAttendee'; 
 
 @Component({
     moduleId: module.id,
@@ -6,28 +9,21 @@
     templateUrl: 'test-report.html'
 })
 
-export class TestReportComponent {
-
+export class TestReportComponent implements OnInit {
     showSearchInput: boolean;
-
-    /**
-     * Data for Report Table
-     */
-    reportData: Array<Object> = [
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 01 2017', score: 125, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 02 2017', score: 135, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 03 2017', score: 126, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 01 2018', score: 127, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 04 2017', score: 128, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 05 2017', score: 129, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 06 2017', score: 145, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 07 2017', score: 155, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 08 2017', score: 165, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 09 2017', score: 175, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 10 2017', score: 185, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 11 2017', score: 195, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 12 2017', score: 125, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 13 2017', score: 125, percentage: '62 %' },
-        { name: 'John Doe', email: 'joh.doe@gmail.com', date: 'Sun Jan 14 2017', score: 125, percentage: '62 %' }
-    ];
+    testAttendeeArray: TestAttendee[];
+    testId: number;
+    constructor(private reportService: ReportService, private route: ActivatedRoute) {
+        this.testAttendeeArray = new Array<TestAttendee>();
+    }
+    ngOnInit() {
+        this.testId = this.route.snapshot.params['id'];
+        this.getAllTestAttendees();
+    }
+    getAllTestAttendees() {
+        this.reportService.getAllTestAttendees(this.testId).subscribe((attendeeList) => {
+            this.testAttendeeArray = attendeeList
+        });
+    }
 }
+
