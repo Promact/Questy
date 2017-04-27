eba1b79bb41fbf63317bfaa3860e76b21e0220df
madhurima <madhurima@PR2C2R6Y2016>
2017-04-27 11:02:56 +0530

    Implemented client side API for view test page.

---
+++ Trappist/src/Promact.Trappist.DomainModel/Enum/BrowserTolerance.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Promact.Trappist.DomainModel.Enum
+{
+    public enum BrowserTolerance
+    {
+        NotApplicable,
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170426140520_ModifiedDataType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class ModifiedDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WarningTime",
+                table: "Test",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WarningTime",
+                table: "Test",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
