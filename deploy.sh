dc2bfec0ba796acd947c8969f600f275fa068bb0
rushi <rushi_216@yahoo.com>
2017-04-27 18:09:38 +0530

    removed adding migration while deploying, shorten commit hash in deployed version

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -e
 
 cd ./Trappist/src/Promact.Trappist.Web
-dotnet ef migrations add prod
 dotnet publish -o published
-git log --format="%H" -n 1 > ./published/.revision
+git log --format="%h" -n 1 > ./published/.revision
 cd ../../../
 docker build -t promact/trappist:$TRAVIS_BRANCH .
 docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
