2c494c17e8e8b577000e65df177d11797707e27e
deepankar <deepankar@promactinfo.com>
2017-04-29 15:08:25 +0530

    Added Model for test conducting

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestConduct/TestConduct.cs
@@ -0,0 +1,20 @@
+﻿using Promact.Trappist.DomainModel.Enum;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.TestConduct
+{
+    public class TestConduct : BaseModel
+    {
+        public int TestAttendeeId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public QuestionStatus QuestionStatus { get; set; }
+
+        [ForeignKey("TestAttendeeId")]
+        public virtual TestAttendees TestAttendees { get; set; }
+
+        [ForeignKey("QuestionId")]
+        public virtual Question.Question Question { get; set; }
+    }
+}
