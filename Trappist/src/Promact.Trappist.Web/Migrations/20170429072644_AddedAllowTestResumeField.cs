1d1f928964d7dc853beba30adf73879c51789eda
madhurima <madhurima@PR2C2R6Y2016>
2017-04-29 13:25:24 +0530

    Added AllowTestResume field in Test model.

---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170429072644_AddedAllowTestResumeField.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class AddedAllowTestResumeField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AllowTestResume",
+                table: "Test",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowTestResume",
+                table: "Test");
+        }
+    }
+}
