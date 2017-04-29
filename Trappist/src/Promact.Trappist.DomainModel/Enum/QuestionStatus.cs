c26e8b5b304146356d20a564c8045b4e6228c763
deepankar <deepankar@promactinfo.com>
2017-04-29 15:08:25 +0530

    Added Model for test conducting

---
+++ Trappist/src/Promact.Trappist.DomainModel/Enum/QuestionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promact.Trappist.DomainModel.Enum
+{
+    public enum QuestionStatus
+    {
+        answered,
+        review,
+        selected,
+        unanswered
+    }
+}
---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestConduct/Answers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promact.Trappist.DomainModel.Models.TestConduct
+{
+    public class Answers
+    {
+        public int AttendeeId { get; set; }
+
+        public int AnsweredOption { get; set; }
+
+
+    }
+}
