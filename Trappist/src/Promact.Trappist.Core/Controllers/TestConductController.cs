8973d1e45c9ef47fcf160b98c24733f1f18549ed
deepankar <deepankar@promactinfo.com>
2017-04-29 10:08:28 +0530

    Added functionality for test to open in new window

--- Trappist/src/Promact.Trappist.Core/Controllers/TestConductController.cs
+++ Trappist/src/Promact.Trappist.Core/Controllers/TestConductController.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Promact.Trappist.DomainModel.ApplicationClasses.TestConduct;
 using Promact.Trappist.DomainModel.Models.TestConduct;
 using Promact.Trappist.Repository.TestConduct;
 using Promact.Trappist.Repository.Tests;
+using System;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Promact.Trappist.Core.Controllers
 {
-    [Authorize]
     [Route("api/conduct")]
     public class TestConductController : Controller
     {
@@ -17,6 +17,8 @@ namespace Promact.Trappist.Core.Controllers
         #region Dependencies
         private readonly ITestConductRepository _testConductRepository;
         private readonly ITestsRepository _testRepository;
+        private const string ATTENDEE_ID = "ATTENDEE_ID";
+        private const string TEST_LINK = "MAGIC_LINK";
         #endregion
         #endregion
 
@@ -41,6 +43,8 @@ namespace Promact.Trappist.Core.Controllers
             if (ModelState.IsValid && !(await _testConductRepository.IsTestAttendeeExistAsync(testAttendee, magicString)))
             {
                 await _testConductRepository.RegisterTestAttendeesAsync(testAttendee, magicString);
+                HttpContext.Session.SetInt32(ATTENDEE_ID, testAttendee.Id);
+                HttpContext.Session.SetString(TEST_LINK, magicString);
                 return Ok(true);
             }
             return BadRequest();
@@ -52,19 +56,59 @@ namespace Promact.Trappist.Core.Controllers
         /// <param name="attendeeId">Id of Test Attendee</param>
         /// <param name="answer">Answer submitted</param>
         [HttpPut("answer/{attendeeId}")]
-        public async Task<IActionResult> AddAnswerAsync([FromRoute]int attendeeId, [FromBody] TestAnswerAC answer)
+        public async Task<IActionResult> AddAnswerAsync([FromRoute]int attendeeId, [FromBody] TestAnswerAC[] answer)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
-            if (!await _testRepository.IsTestAttendeeExistAsync(attendeeId))
+
+            if (HttpContext.Session.GetInt32(ATTENDEE_ID).Value != attendeeId)
+            {
+                return BadRequest();
+            }
+
+            if (!await _testConductRepository.IsTestAttendeeExistByIdAsync(attendeeId))
             {
                 return NotFound();
             }
+                       
             await _testConductRepository.AddAnswerAsync(attendeeId, JsonConvert.SerializeObject(answer));
 
             return Ok(answer);
+        }
+
+        /// <summary>
+        /// Sets the elapsed time from the beginning of Test
+        /// </summary>
+        /// <param name="attendeeId">Id of Attendee</param>
+        [HttpPut("elapsetime/{attendeeId}")]
+        public async Task<IActionResult> SetElapsedTimeAsync([FromRoute]int attendeeId)
+        {
+            if (HttpContext.Session.GetInt32(ATTENDEE_ID).Value != attendeeId)
+            {
+                return BadRequest();
+            }
+
+            await _testConductRepository.SetElapsedTimeAsync(attendeeId);
+
+            return Ok(attendeeId);
+        }
+
+        /// <summary>
+        /// Gets the elapsed time from the beginning of Test
+        /// </summary>
+        /// <param name="attendeeId">Id of Attendee</param>
+        /// <returns>Time elapsed in double</returns>
+        [HttpGet("elapsetime/{attendeeId}")]
+        public async Task<IActionResult> GetElapsedTimeAsync([FromRoute]int attendeeId)
+        {
+            if (HttpContext.Session.GetInt32(ATTENDEE_ID).Value != attendeeId)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _testConductRepository.GetElapsedTimeAsync(attendeeId));
         }
 
         /// <summary>
@@ -75,12 +119,79 @@ namespace Promact.Trappist.Core.Controllers
         [HttpGet("answer/{attendeeId}")]
         public async Task<IActionResult> GetAnswerAsync([FromRoute]int attendeeId)
         {
-            if (!await _testRepository.IsTestAttendeeExistAsync(attendeeId))
+            if (HttpContext.Session.GetInt32(ATTENDEE_ID).Value != attendeeId)
+            {
+                return BadRequest();
+            }
+
+            if (!await _testConductRepository.IsTestAttendeeExistByIdAsync(attendeeId))
             {
                 return NotFound();
             }
 
-            return Ok(await _testConductRepository.GetAnswerAsync(attendeeId));
+            var attendeeAnswers = await _testConductRepository.GetAnswerAsync(attendeeId);
+
+            if (attendeeAnswers == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(attendeeAnswers);
+        }
+
+        /// <summary>
+        /// Gets Test Attendee by Id
+        /// </summary>
+        /// <param name="testId">Id of Test</param>
+        /// <returns>TestAttendee object</returns>
+        [HttpGet("attendee/{testid}")]
+        public async Task<IActionResult> GetTestAttendeeByIdAsync([FromRoute] int testId)
+        {
+            if(!await _testRepository.IsTestExists(testId))
+            {
+                return NotFound();
+            }
+
+            var attendeeId = HttpContext.Session.GetInt32(ATTENDEE_ID).Value;
+
+            if (!await _testConductRepository.IsTestAttendeeExistByIdAsync(attendeeId))
+            {
+                return NotFound();
+            }
+
+            return Ok(await _testConductRepository.GetTestAttendeeByIdAsync(attendeeId));
+        }
+
+        /// <summary>
+        /// Gets all the Question in a Test. Answers of the questions are excluded.
+        /// </summary>
+        /// <param name="id">Id of the Test</param>
+        /// <returns>List of QuestionAC object</returns>
+        [HttpGet("testquestion/{id}")]
+        public async Task<IActionResult> GetTestQuestionByTestIdAsync([FromRoute] int id)
+        {
+            if (!await _testRepository.IsTestExists(id))
+            {
+                return NotFound();
+            }
+
+            return Ok(await _testRepository.GetTestQuestionByTestIdAsync(id));
+        }
+        
+        /// <summary>
+        /// Returns Test by Test Link
+        /// </summary>
+        /// <param name="link">Link of the Test</param>
+        /// <returns>TestAC object</returns>
+        [HttpGet("testbylink/{link}")]
+        public async Task<IActionResult> GetTestByLinkAsync([FromRoute]string link)
+        {
+            if (link == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _testRepository.GetTestByLinkAsync(link));
         }
 
         #region Test-Instruction API
---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/TestConduct/TestConductAC.cs
@@ -0,0 +1,15 @@
+﻿using Promact.Trappist.DomainModel.ApplicationClasses.Question;
+using Promact.Trappist.DomainModel.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.TestConduct
+{
+    public class TestConductAC
+    {
+        public QuestionAC Question { get; set; }
+
+        public QuestionStatus QuestionStatus { get; set; }
+    }
+}
