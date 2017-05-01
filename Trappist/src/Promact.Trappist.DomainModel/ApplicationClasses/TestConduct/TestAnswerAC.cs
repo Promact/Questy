270ab32e9fb75be904101efd05ad09ffa4add7dc
deepankar <deepankar@promactinfo.com>
2017-05-01 17:42:26 +0530

    Added Test Answer application class, Fixed  code formatting issue
    Added Json string for function testing
    
    Using IEnumerable instead of int array, fixed a comment.

---
+++ Trappist/src/Promact.Trappist.DomainModel/ApplicationClasses/TestConduct/TestAnswerAC.cs
@@ -0,0 +1,19 @@
+﻿using Promact.Trappist.DomainModel.Enum;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Promact.Trappist.DomainModel.ApplicationClasses.TestConduct
+{
+    public class TestAnswerAC
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        public IEnumerable<int> OptionChoice { get; set; }
+
+        public string Code { get; set; }
+
+        public QuestionStatus QuestionStatus { get; set; }
+    }
+}
