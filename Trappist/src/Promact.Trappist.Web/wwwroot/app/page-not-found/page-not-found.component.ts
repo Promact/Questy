2dd67e0f31ac7db2cfe019045a92da8f4ba8a806
madhurima <madhurima@PR2C2R6Y2016>
2017-05-01 09:50:56 +0530

    Solved bug #21869.

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'page-not-found',
+    templateUrl: 'page-not-found.html'
+})
+
+export class PageNotFoundComponent {
+
+    constructor(){}
+}
