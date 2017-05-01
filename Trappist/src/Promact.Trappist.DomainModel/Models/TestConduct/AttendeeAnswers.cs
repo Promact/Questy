bda9855062ea9e302908456fb36828770d8b67e3
deepankar <deepankar@promactinfo.com>
2017-05-01 08:38:58 +0530

    Added db model for storing answers

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestConduct/AttendeeAnswers.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.TestConduct
+{
+    public class AttendeeAnswers : BaseModel
+    {
+        public string Answers { get; set; }
+
+        [ForeignKey("Id")]
+        public virtual TestAttendees TestAttendees { get; set; }
+    }
+}
