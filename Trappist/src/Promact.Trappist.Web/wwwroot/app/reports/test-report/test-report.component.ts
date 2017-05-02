b39b85f4c1928075a7149ee994ca592a755135cf
Sandipan Ghosh <sandipan@promactinfo.com>
2017-05-02 09:25:01 +0530

    implemented functionality of showing all candidates, applied region in controller and repository.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/reports/test-report/test-report.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/reports/test-report/test-report.component.ts
@@ -2,7 +2,7 @@
 import { ReportService } from '../report.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TestAttendee } from '../testAttendee';
-import { TestState } from '../enum-test-state';
+import { TestStatus } from '../enum-test-state';
 
 @Component({
     moduleId: module.id,
@@ -19,45 +19,49 @@ export class TestReportComponent implements OnInit {
     starredCandidateArray: string[];
     typeOfTest: string;
     count: number;
-    testState: TestState;
-    selectedTestState: TestState;
+    selectedTestStatus: TestStatus;
     headerStarStatus: string;
     rowStarStatus: string;
     activePage: number;
     starredCandidateIdList: number[];
-    selectedCandidateIdList: number[];
-    isCandidateStarred: boolean;
     isAllCandidateStarred: boolean;
+    showStarCandidates: boolean;
 
     constructor(private reportService: ReportService, private route: ActivatedRoute) {
         this.testAttendeeArray = new Array<TestAttendee>();
         this.attendeeArray = new Array<TestAttendee>();
         this.typeOfTest = '0';
-        this.selectedTestState = TestState.allCandidates;
+        this.selectedTestStatus = TestStatus.allCandidates;
         this.searchString = '';
-        //this.starredCandidateArray = new Array<string>();
         this.headerStarStatus = 'star_border';
         this.rowStarStatus = 'star_border';
         this.activePage = 1;
         this.starredCandidateIdList = new Array<number>();
-        this.selectedCandidateIdList = new Array<number>();
         this.isAllCandidateStarred = false;
-        this.isCandidateStarred = false;
-    }
-    ngOnInit() {
-        this.testId = this.route.snapshot.params['id'];
-        this.getAllTestAttendees();
+        this.showStarCandidates = false;
     }
 
-    getAllTestAttendees() {
+    ngOnInit() {
+        this.testId = this.route.snapshot.params['id'];
+        this.getAllTestCandidates();
+    }
+
+    /**
+     * Fetches all the candidates of any particular test
+     */
+    getAllTestCandidates() {
         this.reportService.getAllTestAttendees(this.testId).subscribe((attendeeList) => {
             this.attendeeArray = attendeeList;
             this.testAttendeeArray = this.attendeeArray;
-            [this.headerStarStatus, this.isAllCandidateStarred, this.isCandidateStarred] = this.testAttendeeArray.some(x => !x.starredCandidate) ? ['star_border', false, false] : ['star', true, true];
+            [this.headerStarStatus, this.isAllCandidateStarred] = this.testAttendeeArray.some(x => !x.starredCandidate) ? ['star_border', false] : ['star', true];
             this.countAttendees();
         });
     }
 
+    /**
+     * Sets all the candidates as starred candidates
+     * @param testId
+     */
     setAllCandidatesStarred(testId: number) {
         for (let i = 0; i < this.testAttendeeArray.length; i++) {
             this.starredCandidateIdList[i] = this.testAttendeeArray[i].id;
@@ -66,52 +70,105 @@ export class TestReportComponent implements OnInit {
         this.reportService.setAllCandidatesStarred(this.testId, status, this.starredCandidateIdList).subscribe(
             response => {
                 this.testAttendeeArray.forEach(k => k.starredCandidate = status);
+                [this.headerStarStatus, this.isAllCandidateStarred] = status ? ['star', true] : ['star_border', false];
             },
             err => {
 
             }
         );
-        [this.headerStarStatus, this.isAllCandidateStarred, this.isCandidateStarred] = status ? ['star', true, true] : ['star_border', false, false];
+       
     }
 
+    /**
+     * Toggles the star condition of a candidate
+     * @param testAttendee
+     */
     setStarredCandidate(testAttendee: TestAttendee) {
         this.reportService.setStarredCandidate(testAttendee.id).subscribe(
             response => {
                 this.testAttendeeArray.find(x => x.id === testAttendee.id).starredCandidate = !testAttendee.starredCandidate;
-                [this.headerStarStatus, this.isAllCandidateStarred, this.isCandidateStarred] = this.testAttendeeArray.some(x => !x.starredCandidate) ? ['star_border', false, false] : ['star', true, true];
+                [this.headerStarStatus, this.isAllCandidateStarred] = this.testAttendeeArray.some(x => !x.starredCandidate) ? ['star_border', false] : ['star', true];
             },
             err => {
             }
         );
     }
+
+    /**
+     * Checks whther a candidate is satrred
+     * @param attendee
+     */
     isStarredCandidate(attendee: TestAttendee) {
-        [this.rowStarStatus, this.isCandidateStarred] = attendee.starredCandidate ? ['star', true] : ['star_border', false];
-        return this.rowStarStatus;
+        return attendee.starredCandidate ? 'star' : 'star_border';
     }
 
+    /**
+     * Displays only the starred candidates
+     */
+    showAllStarredCandidates() {
+        this.showStarCandidates = !this.showStarCandidates;
+        this.filterList();
+    }
+
+    /**
+     * Sets the search criteria
+     * @param searchString
+     */
     getTestAttendeeMatchingSearchCriteria(searchString: string) {
         this.searchString = searchString;
-        this.filterAttendeeList();
+        this.filterList();
     }
 
-    setTypeOfTest(typeOfTest: string) {
-        this.selectedTestState = +typeOfTest;
-        this.filterAttendeeList();
+    /**
+     * Sets the test status of the candidate
+     * @param typeOfTest
+     */
+    setTestStatusType(typeOfTest: string) {
+        this.selectedTestStatus = +typeOfTest;
+        this.filterList();
     }
 
-    filter(selectedTestState: TestState, searchString: string) {
+    /**
+     * Initiates filtering of candidates
+     */
+    filterList() {
+        this.filter(this.selectedTestStatus, this.searchString,this.showStarCandidates);
+        [this.headerStarStatus, this.isAllCandidateStarred] = (this.testAttendeeArray.some(x => !x.starredCandidate) || (this.testAttendeeArray.length === 0)) ? ['star_border', false] : ['star', true];
+        this.countAttendees();
+    }
+
+    /**
+     * Filters the test attendee list based on test status of the candidate and search string provided by user
+     * @param selectedTestStatus
+     * @param searchString
+     */
+    filter(selectedTestStatus: TestStatus, searchString: string, showStarCandidates: boolean) {
         let tempAttendeeArray: TestAttendee[] = [];
         searchString = searchString.toLowerCase();
         this.testAttendeeArray = [];
+        let starAttendeeArray: TestAttendee[] = [];
 
-        if (selectedTestState === 0) {
-            this.attendeeArray.forEach(x => {
+        if (this.showStarCandidates) {
+            this.attendeeArray.forEach(k => {
+                if (k.starredCandidate)
+                    starAttendeeArray.push(k);
+            });
+        }
+
+        if (!this.showStarCandidates) {
+            this.attendeeArray.forEach(k => {
+                starAttendeeArray.push(k);
+            });
+        }
+
+        if (selectedTestStatus === 0) {
+            starAttendeeArray.forEach(x => {
                 tempAttendeeArray.push(x);
             });
         }
 
-        this.attendeeArray.forEach(x => {
-            if (x.testState === selectedTestState) {
+        starAttendeeArray.forEach(x => {
+            if (x.testStatus === selectedTestStatus) {
                 tempAttendeeArray.push(x);
             }
         });
@@ -125,22 +182,29 @@ export class TestReportComponent implements OnInit {
         });
     }
 
-    filterAttendeeList() {
-        this.filter(this.selectedTestState, this.searchString);
-        [this.headerStarStatus, this.isAllCandidateStarred, this.isCandidateStarred] = this.testAttendeeArray.some(x => !x.starredCandidate) ? ['star_border', false, false] : ['star', true, true];
-        this.countAttendees();
-    }
-
+    /**
+     * Counts the number of candidates
+     */
     countAttendees() {
         return this.count = this.testAttendeeArray.length;
     }
 
-    pageHandling(activePage: number, count: number) {
+    /**
+     * Keeps tarck of starting index of the active page
+     * @param activePage
+     * @param count
+     */
+    startingIndexOfActivePage(activePage: number, count: number) {
 
         return count === 0 ? 0 : (activePage - 1) * 10 + 1;
     }
 
-    pageHandling2(activePage: number, count: number) {
+    /**
+     * Keeps track of the last index of the active page
+     * @param activePage
+     * @param count
+     */
+    endingIndexOfActivePage(activePage: number, count: number) {
         let pagetracker = activePage * 10;
         return pagetracker > count ? count : pagetracker;
     }
