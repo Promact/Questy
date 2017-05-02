26acc7cdd8b88f5780197991a50c67a029351005
Sandipan Ghosh <sandipan@promactinfo.com>
2017-05-02 12:21:03 +0530

    Added time taken by test attendee column

---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170502070421_ChangedDataTypeOfTimeTakenByAttendeFromDateTimeToDecimal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class ChangedDataTypeOfTimeTakenByAttendeFromDateTimeToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeTakenByTheAttendee",
+                table: "Report");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TimeTakenByAttendee",
+                table: "Report",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeTakenByAttendee",
+                table: "Report");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeTakenByTheAttendee",
+                table: "Report",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
