a32780507d63e996dd2dc10d1df9d4c081b72e3e
Sandipan Ghosh <sandipan@promactinfo.com>
2017-05-03 16:54:57 +0530

    updated migration files

---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170503112336_ImplementedReportsTableAndUpdatedTestAttendeeTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class ImplementedReportsTableAndUpdatedTestAttendeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "StarredCandidate",
+                table: "TestAttendees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    Percentage = table.Column<double>(nullable: false),
+                    Percentile = table.Column<double>(nullable: false),
+                    TestAttendeeId = table.Column<int>(nullable: false),
+                    TestStatus = table.Column<int>(nullable: false),
+                    TimeTakenByAttendee = table.Column<int>(nullable: false),
+                    TotalMarksScored = table.Column<double>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_TestAttendees_TestAttendeeId",
+                        column: x => x.TestAttendeeId,
+                        principalTable: "TestAttendees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_TestAttendeeId",
+                table: "Report",
+                column: "TestAttendeeId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+
+            migrationBuilder.DropColumn(
+                name: "StarredCandidate",
+                table: "TestAttendees");
+        }
+    }
+}
