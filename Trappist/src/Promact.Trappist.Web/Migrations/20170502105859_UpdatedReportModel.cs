0909b2fd09475b67bb72ffba76343d121d3b9e92
Sandipan Ghosh <sandipan@promactinfo.com>
2017-05-03 08:53:04 +0530

    modified setAllCandidateStarred logic, modified routing, modified datatype of total marks, percentage and percentile field of reports model.

---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170502105859_UpdatedReportModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class UpdatedReportModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalMarksScored",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Percentile",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Percentage",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalMarksScored",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Percentile",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Percentage",
+                table: "Report",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
