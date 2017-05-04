ff07ff095c0bca274b044b5467004edf95264619
Niharika Dubey <niharika@promactinfo.com>
2017-05-04 15:00:28 +0530

    added IsLaunched and IsPaused properties to Test

--- Trappist/src/Promact.Trappist.DomainModel/Models/Test/Test.cs
+++ Trappist/src/Promact.Trappist.DomainModel/Models/Test/Test.cs
@@ -27,6 +27,8 @@ namespace Promact.Trappist.DomainModel.Models.Test
         public decimal IncorrectMarks { get; set; }
         public TestOrder QuestionOrder { get; set; }
         public TestOrder OptionOrder { get; set; }
+        public bool IsLaunched { get; set; }
+        public bool IsPaused { get; set; }
         public AllowTestResume AllowTestResume { get; set; }
         public virtual ICollection<TestAttendees> TestAttendees { get; set; }
         public virtual ICollection<TestQuestion> TestQuestion { get; set; }
