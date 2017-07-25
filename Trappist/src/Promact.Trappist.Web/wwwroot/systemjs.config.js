0bf6e319258dd60154093af895e3bd1c6ce9e334
Asif Khan <asifkhan.ak95.ak@gmail.com>
2017-07-25 15:58:34 +0530

    solved loader problem in test.questions.component

--- Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
+++ Trappist/src/Promact.Trappist.Web/wwwroot/systemjs.config.js
@@ -49,7 +49,7 @@
             'angular2-infinite-scroll': 'npm:angular2-infinite-scroll',
             'angular2-tinymce': 'npm:angular2-tinymce/dist',
             'tinymce': 'npm:tinymce',
-            'ngx-popover': 'npm:ngx-popover',
+            'ngx-popover': 'node_modules/ngx-popover',
             '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
