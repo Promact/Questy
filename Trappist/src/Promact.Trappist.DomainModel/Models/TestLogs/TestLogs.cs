03f9c4ee6c4fbf5186df7c502bb8c4513f7e2f14
madhurima <madhurima@DESKTOP-9A4PC33>
2017-07-28 16:01:38 +0530

    Added Model for test logs.

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestLogs/TestLogs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.TestLogs
+{
+    public class TestLogs : BaseModel
+    {
+        [Required]
+        public DateTime VisitTestLink { get; set; }
+
+        [Required]
+        public DateTime FillRegistrationForm { get; set; }
+
+        [Required]
+        public DateTime StartTest { get; set; }
+
+        [Required]
+        public DateTime? FinishTest { get; set; }
+
+        public DateTime? AwayFromTestWindow { get; set; }
+
+        public DateTime? DisconnectedFromServer { get; set; }
+
+        public DateTime? CloseWindowWithoutFinishingTest { get; set; }
+
+        public DateTime? ResumeTest { get; set; }
+
+        public int TestAttendeeId { get; set; }
+
+        [ForeignKey("TestAttendeeId")]
+        public virtual TestConduct.TestAttendees TestAttendee { get; set; }
+    }
+}
---
+++ Trappist/src/Promact.Trappist.Web/Migrations/20170728102437_AddedModelForTestLogs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Promact.Trappist.Web.Migrations
+{
+    public partial class AddedModelForTestLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AwayFromTestWindow = table.Column<DateTime>(nullable: true),
+                    CloseWindowWithoutFinishingTest = table.Column<DateTime>(nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    DisconnectedFromServer = table.Column<DateTime>(nullable: true),
+                    FillRegistrationForm = table.Column<DateTime>(nullable: false),
+                    FinishTest = table.Column<DateTime>(nullable: false),
+                    ResumeTest = table.Column<DateTime>(nullable: true),
+                    StartTest = table.Column<DateTime>(nullable: false),
+                    TestAttendeeId = table.Column<int>(nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(nullable: true),
+                    VisitTestLink = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestLogs_TestAttendees_TestAttendeeId",
+                        column: x => x.TestAttendeeId,
+                        principalTable: "TestAttendees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestLogs_TestAttendeeId",
+                table: "TestLogs",
+                column: "TestAttendeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestLogs");
+        }
+    }
+}
