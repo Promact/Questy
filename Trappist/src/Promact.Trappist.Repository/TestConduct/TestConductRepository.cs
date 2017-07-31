36971f80cbababe8d33a72c7209a5c1d31f4a5f1
Deepankar Sinha <nix.sinha@gmail.com>
2017-07-31 19:13:03 +0530

    Fixed unit testing

--- Trappist/src/Promact.Trappist.Repository/TestConduct/TestConductRepository.cs
+++ Trappist/src/Promact.Trappist.Repository/TestConduct/TestConductRepository.cs
@@ -255,7 +255,6 @@ namespace Promact.Trappist.Repository.TestConduct
                 {
                     allTestCasePassed = false;
                 }
-                
             }
             return allTestCasePassed;
         }
--- Trappist/src/Promact.Trappist.Test/TestConduct/TestConductRepositoryTest.cs
+++ Trappist/src/Promact.Trappist.Test/TestConduct/TestConductRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CodeBaseSimulator.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -342,8 +343,13 @@ namespace Promact.Trappist.Test.TestConduct
 
             answer = new TestAnswerAC()
             {
-                OptionChoice = new List<int>() { },
+                OptionChoice = new List<int>(),
                 QuestionId = 2,
+                Code = new Code(){
+                    Input = "input",
+                    Source = "source",
+                    Language = ProgrammingLanguage.C
+                },
                 QuestionStatus = QuestionStatus.answered
             };
             await _testConductRepository.AddAnswerAsync(attendeeId, answer);
