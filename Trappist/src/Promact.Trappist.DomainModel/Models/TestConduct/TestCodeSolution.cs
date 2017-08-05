60ce5693fb44262473dbdf013bdda5befcc800bf
Deepankar Sinha <nix.sinha@gmail.com>
2017-08-05 16:33:11 +0530

    Added functionality to assess code snippet question

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestConduct/TestCodeSolution.cs
@@ -0,0 +1,24 @@
+﻿using Promact.Trappist.DomainModel.Enum;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.TestConduct
+{
+    public class TestCodeSolution: BaseModel
+    {
+        public int TestAttendeeId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public string Solution { get; set; }
+
+        public ProgrammingLanguage Language { get; set; }
+
+        public double Score { get; set; }
+
+        [ForeignKey("TestAttendeeId")]
+        public virtual TestAttendees TestAttendee { get; set; }
+
+        [ForeignKey("QuestionId")]
+        public virtual Question.Question Question { get; set; }
+    }
+}
