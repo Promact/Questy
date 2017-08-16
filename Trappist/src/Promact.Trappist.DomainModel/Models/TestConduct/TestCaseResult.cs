2e02aaa140f2f604733c3767155afa847d59504f
Deepankar Sinha <nix.sinha@gmail.com>
2017-08-16 11:18:12 +0530

    Fixed code snippet question page and added model for storing test case result

---
+++ Trappist/src/Promact.Trappist.DomainModel/Models/TestConduct/TestCaseResult.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Promact.Trappist.DomainModel.Models.TestConduct
+{
+    public class TestCaseResult: BaseModel
+    {
+        public long Processing { get; set; }
+
+        public long Memory { get; set; }
+
+        public string Output { get; set; }
+
+        public int TestCodeSolutionId { get; set; }
+
+        [ForeignKey("TestCodeSolutionId")]
+        public virtual TestCodeSolution TestCodeSolution { get; set; }
+    }
+}
