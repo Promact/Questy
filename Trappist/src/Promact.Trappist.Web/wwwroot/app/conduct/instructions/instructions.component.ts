cd3f0ca8cb8f905f9efb7783467a8d77af5df787
suparna <suparna@DESKTOP-Q7TIUDF>
2017-08-24 10:12:54 +0530

    changed loader position to excel and instruction page

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/instructions/instructions.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/conduct/instructions/instructions.component.ts
@@ -28,8 +28,9 @@ export class InstructionsComponent implements OnInit {
      *Gets the link from the url and pass it as parameter to a specific method to fetch all the information of a particular test
      */
     ngOnInit() {
+        this.loader = true;
         let url = window.location.pathname;
-        this.magicString = url.substring(url.indexOf('/conduct/') + 9, url.indexOf('/instructions'));
+        this.magicString = url.substring(url.indexOf('/conduct/') + 9, url.indexOf('/instructions'));   
         this.getTestInstructionsByLink(this.magicString);
     }
 
@@ -38,7 +39,6 @@ export class InstructionsComponent implements OnInit {
      * @param testLink Contains the link to fetch instructions related to a particular test
      */
     getTestInstructionsByLink(testLink: string) {
-        this.loader = true;
         this.conductService.getTestInstructionsByLink(testLink).subscribe(
             response => {
                 this.testInstructions = response;
