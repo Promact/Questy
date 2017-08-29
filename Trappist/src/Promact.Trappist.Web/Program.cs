e5f554ad1da68240996a45a6b55e735123a0bb87
Ubuntu <trappist@trappist.gfuoq1ziuuhepmfjz4z2034nfb.jx.internal.cloudapp.net>
2017-08-29 15:55:17 +0000

    Added application insight

--- Trappist/src/Promact.Trappist.Web/Program.cs
+++ Trappist/src/Promact.Trappist.Web/Program.cs
@@ -16,6 +16,7 @@ namespace Promact.Trappist.Web
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
