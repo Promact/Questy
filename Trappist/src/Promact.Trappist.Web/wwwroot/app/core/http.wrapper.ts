d6bb52fe3c8e903496d8055b137d89c1513f8419
rushi <rushi_216@yahoo.com>
2017-08-31 16:19:31 +0530

    added angular 2 support for miniprofiler

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/core/http.wrapper.ts
@@ -0,0 +1,34 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, ConnectionBackend, RequestOptionsArgs, Request, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+declare let MiniProfiler;
+
+@Injectable()
+export class HttpWrapper extends Http {
+
+    constructor(_backend: ConnectionBackend, _defaultOptions: RequestOptions) {
+        super(_backend, _defaultOptions);
+    }
+
+    request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+        return super.request(url, options)
+            .map(r => {
+                if (typeof MiniProfiler !== 'undefined' && r && r.headers) {
+                    this.makeMiniProfilerRequests(r.headers);
+                }
+                return r;
+            });
+    }
+
+    private makeMiniProfilerRequests(headers: Headers) {
+        var miniProfilerHeaders = headers.getAll('x-miniprofiler-ids');
+        if (!miniProfilerHeaders) {
+            return;
+        }
+        miniProfilerHeaders.forEach(miniProfilerIdHeaderValue => {
+            var ids = JSON.parse(miniProfilerIdHeaderValue) as string[];
+            MiniProfiler.fetchResults(ids);
+        });
+    }
+}
