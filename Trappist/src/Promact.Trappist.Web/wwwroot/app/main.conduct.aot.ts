3cfc1f4f2984f54f08104c4c9dd5eca8603a9a10
rushi <rushi_216@yahoo.com>
2017-09-02 14:48:41 +0530

    enabled prod mode in angular

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/main.conduct.aot.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/main.conduct.aot.ts
@@ -1,5 +1,9 @@
-﻿import { platformBrowser } from '@angular/platform-browser';
+﻿import { enableProdMode } from "@angular/core";
+
+import { platformBrowser } from '@angular/platform-browser';
 
 import { ConductModuleNgFactory } from '../aot/app/conduct/conduct.module.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(ConductModuleNgFactory);
+
+enableProdMode();
--- Trappist/src/Promact.Trappist.Web/wwwroot/app/main.setup.aot.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/main.setup.aot.ts
@@ -1,5 +1,9 @@
-﻿import { platformBrowser } from '@angular/platform-browser';
+﻿import { enableProdMode } from "@angular/core";
+
+import { platformBrowser } from '@angular/platform-browser';
 
 import { SetupModuleNgFactory } from '../aot/app/setup/setup.module.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(SetupModuleNgFactory);
+
+enableProdMode();
