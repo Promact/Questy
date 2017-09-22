a7e397e32fe36e248731fa5d56a08e4451e0f9bf
madhurima <madhurima@DESKTOP-9A4PC33>
2017-09-22 09:55:01 +0530

    Written unit testing for the method fetching the list of attendees taking a test.

--- Trappist/src/Promact.Trappist.Test/Reports/ReportsRepositoryTest.cs
+++ Trappist/src/Promact.Trappist.Test/Reports/ReportsRepositoryTest.cs
@@ -227,6 +227,55 @@ namespace Promact.Trappist.Test.Reports
         /// Test case to get all amrks details of all attendees for excel sheet are calculated proper or not
         /// </summary>
         /// <returns>Returns true if return value of actual method is matched with checking value </returns>
+        /// <summary>
+        /// Test case for fetching the list of attendees taking a test
+        /// </summary>
+        [Fact]
+        public async Task GetAttendeeIdListAsyncTest()
+        {
+            var test = CreateTest("Geography");
+            await _testRepository.CreateTestAsync(test, "5");
+            var testAttendee = CreateTestAttendee(test.Id);
+            await _testConductRepository.RegisterTestAttendeesAsync(testAttendee, "Added");
+            var attendeeIdList = await _reportRepository.GetAttendeeIdListAsync(test.Id);
+            Assert.Equal(1, attendeeIdList.Count());
+        }
+
+        private DomainModel.Models.Test.Test CreateTest(string testName)
+        {
+            var test = new DomainModel.Models.Test.Test
+            {
+                TestName = testName,
+                CorrectMarks = 3,
+                IncorrectMarks = 1
+            };
+            return test;
+        }
+
+        private TestAttendees CreateTestAttendee(int testId)
+        {
+            var testAttendee = new TestAttendees()
+            {
+                Id = 1,
+                FirstName = "Ritika",
+                LastName = "Mohata",
+                Email = "ritika@gmail.com",
+                RollNumber = "1",
+                TestId = testId,
+                Report = new DomainModel.Models.Report.Report()
+                {
+                    Id = 1,
+                    TestAttendeeId = 1,
+                    TotalMarksScored = 180,
+                    Percentage = 80,
+                    Percentile = 50,
+                    TestStatus = 0,
+                    TimeTakenByAttendee = 150
+                },
+            };
+            return testAttendee;
+        }
+
         [Fact]
         public async Task GetAllAttendeeMarksDetailsAsyncTest()
         {
