0981638d891ca02df359b90354d06e404dcd5f09
rushi <rushi_216@yahoo.com>
2017-09-26 14:59:12 +0530

    changed file saver path in gulpfile

--- Trappist/src/Promact.Trappist.Web/gulpfile.js
+++ Trappist/src/Promact.Trappist.Web/gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('bundle-shims', function () {
         './node_modules/clipboard/dist/clipboard.js',
         './node_modules/jspdf/dist/jspdf.debug.js',
         './node_modules/jspdf-autotable/dist/jspdf.plugin.autotable.js',
-        './node_modules/filesaver.js/FileSaver.js',
+        './node_modules/file-saver/FileSaver.js',
         './node_modules/exceljs/dist/exceljs.js',
         './node_modules/screenfull/dist/screenfull.js'
     ])
