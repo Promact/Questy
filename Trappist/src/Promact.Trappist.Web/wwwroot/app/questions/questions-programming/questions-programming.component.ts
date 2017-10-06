30a917f0f25886b80be303c54072dc20d4b756e6
suparna <suparna@DESKTOP-Q7TIUDF>
2017-10-06 18:56:03 +0530

    prevent multiple button click

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-programming/questions-programming.component.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/questions/questions-programming/questions-programming.component.ts
@@ -43,6 +43,7 @@ export class QuestionsProgrammingComponent implements OnInit {
     testCaseType: TestCaseType;
     questionId: number;
     loader: boolean;
+    isButtonClicked: boolean;
 
     private successMessage: string = 'Question saved successfully.';
     private failedMessage: string = 'Question failed to save.';
@@ -69,6 +70,7 @@ export class QuestionsProgrammingComponent implements OnInit {
         this.isDefaultTestCaseAdded = true;
         this.isQuestionEmpty = true;
         this.isCkeditorDirtly = false;
+        this.isButtonClicked = false;
     }
 
     ngOnInit() {
@@ -286,6 +288,7 @@ export class QuestionsProgrammingComponent implements OnInit {
      * @param isCodeSnippetFormValid : Validation status of code snippet form
      */
     addCodingQuestion(isCodeSnippetFormValid: boolean) {
+        this.isButtonClicked = true;
         this.isDefaultTestCaseAdded = this.isTestCaseValid();
 
         if (isCodeSnippetFormValid && !this.nolanguageSelected && this.isDefaultTestCaseAdded) {
