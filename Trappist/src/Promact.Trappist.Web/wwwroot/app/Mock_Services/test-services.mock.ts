d7caa91fc8a9d3c0e7d9b17637de17031e80ea81
Asif Khan <asifkhan.ak95.ak@gmail.com>
2017-10-10 10:05:19 +0530

    karma configuaration done  with one unit test

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/Mock_Services/test-services.mock.ts
@@ -0,0 +1,18 @@
+﻿import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { Response, ResponseOptions } from '@angular/http';
+import { MockTestData } from "../Mock_Data/test_data.mock";
+
+
+
+
+export class TestServicesMock {
+
+
+
+    getTests() {
+        return Observable.of(MockTestData);
+    }
+
+
+}
