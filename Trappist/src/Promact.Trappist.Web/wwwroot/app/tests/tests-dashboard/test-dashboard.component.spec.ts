6c268cb9bd3ca8843ffdd76d6885a62a1db1ea96
Asif Khan <asifkhan.ak95.ak@gmail.com>
2017-10-10 10:56:52 +0530

    name changed from test.spec to  test-dashboard.component.spec.ts

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.component.spec.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests-dashboard/test-dashboard.component.spec.ts
@@ -3,22 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { async } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgModule, Directive } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import {
     BrowserDynamicTestingModule, platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import { SharedModule } from "../../shared/shared.module";
 import { RouterModule, Router } from '@angular/router';
 import { FilterPipe } from './test-dashboard.pipe';
 import { QuestionsService } from "../../questions/questions.service";
-import { HttpService } from "../../core/http.service";
 import { Http, HttpModule } from "@angular/http";
 import { TestService } from "../tests.service";
 import { inject } from "@angular/core/testing";
 import { Test } from "../tests.model";
 import { testsRouting } from '../tests.routing';
-import { TestServicesMock } from "../../Mock_Services/test-services.mock";
+import { TestServicesMock, MockQuestionService } from "../../Mock_Services/test-services.mock";
 
 
 
@@ -26,11 +23,7 @@ class RouterStub {
     navigateByUrl(url: string) { return url; }
 }
 
-
-export class MockQuestionService {
-
-}
-describe('test spec', () => {
+describe('Test Dashboard Com[ponent', () => {
     let testDashboard: TestsDashboardComponent;
     let fixture: ComponentFixture<TestsDashboardComponent>;
 
@@ -43,17 +36,12 @@ describe('test spec', () => {
                 { provide: TestService, useClass: TestServicesMock },
                 { provide: Router, useClass: RouterStub }
             ],
-
-
             imports: [BrowserModule, FormsModule, MaterialModule, RouterModule, HttpModule]
         }).compileComponents();
     }));
-
-
     beforeEach(() => {
         fixture = TestBed.createComponent(TestsDashboardComponent);
         testDashboard = fixture.componentInstance;
-
     });
 
     it('getTest', () => {
