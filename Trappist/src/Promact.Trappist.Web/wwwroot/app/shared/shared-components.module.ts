dc405190c72584a3f38f6945ed0472ee86197d70
madhurima <madhurima@DESKTOP-9A4PC33>
2017-10-11 13:25:15 +0530

    Removed unnecessary angular material from conduct module.

---
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/shared/shared-components.module.ts
@@ -0,0 +1,37 @@
+﻿import { NgModule } from '@angular/core';
+import { TestComponent } from '../conduct/test/test.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MdSelectModule, MdDialogModule, MdSnackBarModule, MdCheckboxModule, MdRadioModule } from '@angular/material';
+import { AceEditorModule } from 'ng2-ace-editor';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        MdSelectModule,
+        MdDialogModule,
+        MdSnackBarModule,
+        MdCheckboxModule,
+        MdRadioModule,
+        AceEditorModule
+    ],
+    declarations: [
+        TestComponent,
+        PageNotFoundComponent
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        MdSelectModule,
+        MdDialogModule,
+        MdSnackBarModule,
+        MdCheckboxModule,
+        MdRadioModule,
+        AceEditorModule,
+        TestComponent,
+        PageNotFoundComponent
+    ]
+})
+export class SharedComponentsModule { }
