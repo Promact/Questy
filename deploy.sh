7b7a8b388db324c60e1e710bb0bd6aab0e346ba0
rushi <rushi_216@yahoo.com>
2017-10-16 14:29:19 +0530

    integrated circleci

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ cd ./Trappist/src/Promact.Trappist.Web
 dotnet publish -o published
 git log --format="%h" -n 1 > ./published/.revision
 cd ../../../
-docker build -t promact/trappist:$TRAVIS_BRANCH .
+docker build -t promact/trappist:$CIRCLE_BRANCH .
 docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
-docker push promact/trappist:$TRAVIS_BRANCH
+docker push promact/trappist:$CIRCLE_BRANCH
