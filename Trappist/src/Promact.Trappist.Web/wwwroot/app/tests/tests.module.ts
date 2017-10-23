49b6d0817723a93567fad9f2804bb9758e16d550
madhurima <madhurima@DESKTOP-9A4PC33>
2017-10-23 18:47:16 +0530

    Added code for focusing text area using directive.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.module.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/tests/tests.module.ts
@@ -2,13 +2,12 @@
 import { SharedModule } from '../shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { testsRouting } from './tests.routing';
-
 import { TestsComponent } from './tests.component';
 import { TestsDashboardComponent } from './tests-dashboard/tests-dashboard.component';
 import { TestCreateDialogComponent } from './tests-dashboard/test-create-dialog.component';
 import { DeleteTestDialogComponent } from './tests-dashboard/delete-test-dialog.component';
 import { TestSettingsComponent } from './test-settings/test-settings.component';
-import { RandomQuestionSelectionDialogComponent } from './test-settings/test-launch-dialog.component';
+import { RandomQuestionSelectionDialogComponent } from './test-questions/random-question-selection-dialog.component';
 import { TestSectionsComponent } from './test-sections/test-sections.component';
 import { TestService } from './tests.service';
 import { FilterPipe } from './tests-dashboard/test-dashboard.pipe';
@@ -18,13 +17,10 @@ import { Test } from './tests.model';
 import { TestPreviewComponent } from './test-preview/test-preview.compponent';
 import { CreateTestHeaderComponent } from './shared/create-test-header/create-test-header.component';
 import { CreateTestFooterComponent } from './shared/create-test-footer/create-test-footer.component';
-
 import { DuplicateTestDialogComponent } from './tests-dashboard/duplicate-test-dialog.component';
 import { DeselectCategoryComponent } from './test-sections/deselect-category.component';
 import { IncompleteTestCreationDialogComponent } from './test-settings/incomplete-test-creation-dialog.component';
-
 import { ConductService } from '../conduct/conduct.service';
-
 
 @NgModule({
     imports: [
