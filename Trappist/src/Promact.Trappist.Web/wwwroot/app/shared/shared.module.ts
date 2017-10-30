92c1084f924462906f245e931bb9f5b5d4e53840
madhurima <madhurima@DESKTOP-9A4PC33>
2017-10-30 17:46:42 +0530

    Removed Aceeditor from shared.module.ts.

--- Trappist/src/Promact.Trappist.Web/wwwroot/app/shared/shared.module.ts
+++ Trappist/src/Promact.Trappist.Web/wwwroot/app/shared/shared.module.ts
@@ -7,12 +7,8 @@ import { Md2AccordionModule, Md2DataTableModule } from 'md2';
 import { CKEditorModule } from 'ng2-ckeditor';
 import { ChartsModule } from 'ng2-charts';
 import { PopoverModule } from 'ngx-popover';
-import { AceEditorModule } from 'ng2-ace-editor';
 import { ClipboardModule } from 'ngx-clipboard';
-import { TestComponent } from '../conduct/test/test.component';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 import { SelectTextAreaDirective } from '../tests/directive';
-
 
 
 @NgModule({
@@ -26,12 +22,9 @@ import { SelectTextAreaDirective } from '../tests/directive';
         CKEditorModule,
         PopoverModule,
         ChartsModule,
-        AceEditorModule,
         ClipboardModule,
     ],
     declarations: [
-        TestComponent,
-        PageNotFoundComponent,
         SelectTextAreaDirective
     ],
     exports: [
@@ -42,10 +35,7 @@ import { SelectTextAreaDirective } from '../tests/directive';
         CKEditorModule,
         ChartsModule,
         PopoverModule,
-        AceEditorModule,
         ClipboardModule,
-        TestComponent,
-        PageNotFoundComponent,
         SelectTextAreaDirective
     ]
 })
