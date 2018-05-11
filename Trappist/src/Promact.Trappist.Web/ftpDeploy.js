5b96f05e3a1678bb463eb2793a32aa8cf0628d87
rushi <rushi.2106@gmail.com>
2018-05-11 16:37:55 +0530

    removed cdn storage, will use direct origin

--- Trappist/src/Promact.Trappist.Web/ftpDeploy.js
+++ Trappist/src/Promact.Trappist.Web/ftpDeploy.js
@@ -2,9 +2,9 @@ var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-    username: process.env["FTP_USERNAME_" + process.env.CIRCLE_BRANCH_U],
-    password: process.env["FTP_PASSWORD_" + process.env.CIRCLE_BRANCH_U],
-    host: process.env["FTP_HOST_" + process.env.CIRCLE_BRANCH_U],
+    username: process.env["FTP_USERNAME_" + process.env.CIRCLE_BRANCH.toUpperCase()],
+    password: process.env["FTP_PASSWORD_" + process.env.CIRCLE_BRANCH.toUpperCase()],
+    host: process.env["FTP_HOST_" + process.env.CIRCLE_BRANCH.toUpperCase()],
     port: 21,
     localRoot: __dirname + "/published",
     remoteRoot: "/site/wwwroot",
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-$CIRCLE_BRANCH_U="${CIRCLE_BRANCH^^}"
+CIRCLE_BRANCH_U="${CIRCLE_BRANCH^^}"
 
 cd ./Trappist/src/Promact.Trappist.Web
 dotnet publish -c Release -o published
@@ -9,16 +9,14 @@ git log --format="%h" -n 1 > ./published/.revision
 
 az login --service-principal -u $AZURE_USER -p $AZURE_PASSWORD --tenant $AZURE_TENANT
 
-az storage blob upload-batch --destination $AZURE_CONTAINER_NAME$CIRCLE_BRANCH_U --source published/wwwroot
-
 #Deploy to Azure
 
-az webapp stop --name trappist-dev-win --resource-group trappist-dev-win
+az webapp stop --name trappist-dev-win --resource-group trappist-dev
 
-npm install ftp-deploy
+npm install ftp-deploy@1.2.2
 node ftpDeploy.js
 
-az webapp start --name trappist-dev-win --resource-group trappist-dev-win
+az webapp start --name trappist-dev-win --resource-group trappist-dev
 
 #Deploy to Dockerhub
 #cd ../../../
