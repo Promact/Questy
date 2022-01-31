6c3ebab40664423d7dda483ae2b3f692469f84de
Chintan Shah <chintanshah@promactinfo.com>
2022-02-01 01:08:24 +0530

    fixes randomness issue and removes all razor files

--- Trappist/src/Promact.Trappist.Utility/GlobalUtil/GlobalUtil.cs
+++ Trappist/src/Promact.Trappist.Utility/GlobalUtil/GlobalUtil.cs
@@ -13,7 +13,7 @@ namespace Promact.Trappist.Utility.GlobalUtil
         }
         public string GenerateRandomString(int length)
         {
-            var random = new Random();
+            var random = new Random(DateTime.UtcNow.Millisecond);
             string charactersForRandomString = _stringConstants.CharactersForLink;
             //generating random string by using a predefined set of characters, random.Next selects the next element in the link from the set of characters provided.
             var randomString = new string(Enumerable.Repeat(charactersForRandomString, length).Select(s => s[random.Next(s.Length)]).ToArray());
